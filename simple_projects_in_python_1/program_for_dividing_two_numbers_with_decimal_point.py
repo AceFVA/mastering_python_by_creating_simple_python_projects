d9828d909509c4a1429ba064d32f1a61b29de605
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 17:56:19 +0800

    Added two variables with float()

--- simple_projects_in_python_1/program_for_dividing_two_numbers_with_decimal_point.py
+++ simple_projects_in_python_1/program_for_dividing_two_numbers_with_decimal_point.py
@@ -1,6 +1,10 @@
 # Create a program that ask user to input 2 numb>
 
 # ask the user to input 2 numbers
+num_1 = float(input("Input your first number: "))
+num_2 = float(input("Input your second number: "))
+
+
 # check if the second number is not equal to zero
 # num_1 / num_2
 # print the quotient with decimal point
