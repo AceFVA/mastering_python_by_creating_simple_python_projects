276dfe1960bf6a5612dc7aef1bd9a8bf2b78c1ac
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 18:00:00 +0800

    Added a while loop for verifying if num_2 is equal to zero or not, preventing ZeroDivisionError

--- simple_projects_in_python_1/program_for_dividing_two_numbers_with_decimal_point.py
+++ simple_projects_in_python_1/program_for_dividing_two_numbers_with_decimal_point.py
@@ -6,5 +6,9 @@ num_2 = float(input("Input your second number: "))
 
 
 # check if the second number is not equal to zero
+while num_2 == 0:
+    print("Cannot divide by zero.")
+    num_2 = float(input("Input your second number: "))
+
 # num_1 / num_2
 # print the quotient with decimal point
