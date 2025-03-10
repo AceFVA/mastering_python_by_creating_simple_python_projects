b201bd86400f664f4a68275dae02f42299fdd3d5
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 22:14:43 +0800

    Created for loop

--- simple_projects_in_python_1/program_for_numbers_from_0_to_100_except_ending_with_zero.py
+++ simple_projects_in_python_1/program_for_numbers_from_0_to_100_except_ending_with_zero.py
@@ -1,5 +1,10 @@
 # Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
 
 # create a for loop and iterate numbers from 0 to 100
+for i in range(101):
+
 # if the remainder of i divided by 10 is not equal to 0, then it doesn't ends with zero
+    if i % 10 != 0:
+
 # print i if not ending with zero
+        print(i)
