a1dc8bd261658f1e9c28410e4ee5b0211838518d
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 21:23:50 +0800

    Added pseudocode

---
+++ simple_projects_in_python_1/program_for_counting_odd_numbers.py
@@ -0,0 +1,9 @@
+# Create a program that ask user to input 10 numbers. Print how many are odd numbers.
+
+# ask the user to input 10 numbers with spaces in between
+# create a list of numbers entered by the user
+# set a variable with an initialized value of 0 as a container for the total number of odds
+# create a for loop and iterate an item on the given list
+# if the remainder of the number divided by 2 is equal to 1, then that number is odd
+# odd + 1
+# print the total number of odds
