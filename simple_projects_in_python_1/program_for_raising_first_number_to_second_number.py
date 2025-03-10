4f4a01c305ae734986f98be2da96ffeccb701efb
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 18:49:33 +0800

    Added the pseudocode

---
+++ simple_projects_in_python_1/program_for_raising_first_number_to_second_number.py
@@ -0,0 +1,5 @@
+# Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+# ask the user to input 2 numbers
+# num_1 ** num_2
+# print the power
