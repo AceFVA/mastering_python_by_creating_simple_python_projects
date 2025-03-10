86636087eae3f14d00f67c96b002703f4f2ac508
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 16:46:50 +0800

    Added two variables with input()

--- simple_projects_in_python_1/program_for_printing_bigger_numbers.py
+++ simple_projects_in_python_1/program_for_printing_bigger_numbers.py
@@ -1,6 +1,8 @@
 # Create a program that ask user to input 2 numbers. Print the bigger number.
 
 # ask the user to input 2 numbers
+num_1 = int(input("Input your first number: "))
+num_2 = int(input("Input your second number: "))
 # if num_1 is larger than num_2:
 # print num_1
 # if num_2 is larger than num_1:
