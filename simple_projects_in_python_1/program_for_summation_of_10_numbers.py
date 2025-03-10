37285352baa6e1b2921682962d331e1e2dce200f
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 20:33:14 +0800

    Added pseudocode

---
+++ simple_projects_in_python_1/program_for_summation_of_10_numbers.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+# ask the user to input 10 numbers with spaces in between
+# create a list of numbers entered by the user
+# set a variable with an initialized value of 0 as a container for the summation of numbers
+# create a for loop and iterate an item on the given list
+# summation += num_list[i]
+#print summation
