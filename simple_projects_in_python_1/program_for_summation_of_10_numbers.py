6bb3591594957d514459f04bdd7e27ea2cca5b45
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 20:50:12 +0800

    Added variables that requires an input from user and make it as a list

--- simple_projects_in_python_1/program_for_summation_of_10_numbers.py
+++ simple_projects_in_python_1/program_for_summation_of_10_numbers.py
@@ -1,7 +1,12 @@
 # Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 # ask the user to input 10 numbers with spaces in between
+numbers = input("Enter 10 numbers with spaces in between: ")
+# example: "1 2 3 4 5 6 7 8 9 10"
+
 # create a list of numbers entered by the user
+num_list = numbers.split()
+
 # set a variable with an initialized value of 0 as a container for the summation of numbers
 # create a for loop and iterate an item on the given list
 # summation += num_list[i]
