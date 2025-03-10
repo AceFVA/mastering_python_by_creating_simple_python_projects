adf296818b5e6a6d1d24e7d3abf854070969688b
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 18:53:01 +0800

    Added the main code that directly prints the power of num_1 raised to the num_2

--- simple_projects_in_python_1/program_for_raising_first_number_to_second_number.py
+++ simple_projects_in_python_1/program_for_raising_first_number_to_second_number.py
@@ -1,5 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
 
 # ask the user to input 2 numbers
+num_1 = int(input("Input your first number: "))
+num_2 = int(input("Input your second number: "))
+
 # num_1 ** num_2
 # print the power
+print("Result: ", num_1 ** num_2)
