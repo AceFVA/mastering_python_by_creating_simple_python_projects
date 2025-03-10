a940cb45d34d71a669dea43d13a7d4bbaae5f0a8
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 16:37:25 +0800

    Created a pseudocode in program_for_printing_bigger_numbers.py

---
+++ simple_projects_in_python_1/program_for_printing_bigger_numbers.py
@@ -0,0 +1,7 @@
+# Create a program that ask user to input 2 numbers. Print the bigger number.
+
+# ask the user to input 2 numbers
+# if num_1 is larger than num_2:
+# print num_1
+# if num_2 is larger than num_1:
+# print num_2
