90e3a14f57ac536dedb2b4b011d842fe70e6ae2f
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 17:40:46 +0800

    Created the code that multiplies the two numbers, given by the user, and prints the result at the same time

--- simple_projects_in_python_1/program_for_multiplying_two_numbers.py
+++ simple_projects_in_python_1/program_for_multiplying_two_numbers.py
@@ -1,5 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 
-# ask the user to input 2 numbers
+# ask the user to input 2 number
+num_1 = int(input("Input your first number: "))
+num_2 = int(input("Input your second number: "))
+
 # multiply num_1 with num_2
 # print the product
+print("Result: ", num_1 * num_2)
