5c46e71eda7dec27dd0e5b86a50e93572c06752f
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 17:29:25 +0800

    Changed the displaying message

--- simple_projects_in_python_1/program_for_printing_bigger_numbers.py
+++ simple_projects_in_python_1/program_for_printing_bigger_numbers.py
@@ -7,9 +7,9 @@ num_2 = int(input("Input your second number: "))
 # if num_1 is larger than num_2:
 if num_1 > num_2:
 # print num_1
-    print(num_1)
+    print(num_1, "is larger than", num_2)
 
 # if num_2 is larger than num_1:
 else:
 # print num_2
-    print(num_2)
+    print(num_2, "is larger than", num_1)
