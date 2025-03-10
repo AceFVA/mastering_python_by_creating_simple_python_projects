73742769f18b64a4dbaaa38969d78e2a352d538c
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 22:12:50 +0800

    Added pseudocode

---
+++ simple_projects_in_python_1/program_for_numbers_from_0_to_100_except_ending_with_zero.py
@@ -0,0 +1,5 @@
+# Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
+
+# create a for loop and iterate numbers from 0 to 100
+# if the remainder of i divided by 10 is not equal to 0, then it doesn't ends with zero
+# print i if not ending with zero
