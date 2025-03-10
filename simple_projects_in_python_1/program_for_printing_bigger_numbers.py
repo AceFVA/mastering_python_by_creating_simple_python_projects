ece381ae3b9edde3b9c2490c2d83c1f7769bb0a9
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 17:07:54 +0800

    Edited file program_for_printing_bigger_numbers.py

--- simple_projects_in_python_1/program_for_printing_bigger_numbers.py
+++ simple_projects_in_python_1/program_for_printing_bigger_numbers.py
@@ -3,10 +3,12 @@
 # ask the user to input 2 numbers
 num_1 = int(input("Input your first number: "))
 num_2 = int(input("Input your second number: "))
+
 # if num_1 is larger than num_2:
 if num_1 > num_2:
 # print num_1
     print(num_1)
+
 # if num_2 is larger than num_1:
 else:
 # print num_2
