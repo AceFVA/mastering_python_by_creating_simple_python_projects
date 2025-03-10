1f57fc2c6d28ea73aee1dbcd871decfb25410970
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 17:05:57 +0800

    Added two variables with input()

--- simple_projects_in_python_1/program_for_determining_equal_numbers.py
+++ simple_projects_in_python_1/program_for_determining_equal_numbers.py
@@ -1,5 +1,8 @@
 # Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
 
 # ask the user to input 2 numbers
+num_1 = int(input("Input your first number: "))
+num_2= int(input("Input your second number: "))
+
 # if num_1 is equal to num_2:
 # print Equal
