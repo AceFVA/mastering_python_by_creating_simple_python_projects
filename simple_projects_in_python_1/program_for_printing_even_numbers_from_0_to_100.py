df88e752a969105841e6971e872ecd0b4a13758b
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 21:58:57 +0800

    Transferred the python script from simple_projects_in_python_1/program_for_printing_even_numbers_from_0_to_100.p to simple_projects_in_python_1/program_for_printing_even_numbers_from_0_to_100.py

---
+++ simple_projects_in_python_1/program_for_printing_even_numbers_from_0_to_100.py
@@ -0,0 +1,10 @@
+# Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+# create a for loop and iterate numbers from 0 to 100
+for i in range(101):
+
+# if the remainder of i is equal to 0 when divided by 2, then i is even
+    if i % 2 == 0:
+
+# print i if even
+        print(i)
