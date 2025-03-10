5062619d4c3eee7b26d6468e4e274f3f159dd60c
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 17:55:22 +0800

    Added pseudocode

---
+++ simple_projects_in_python_1/program_for_dividing_two_numbers_with_decimal_point.py
@@ -0,0 +1,6 @@
+# Create a program that ask user to input 2 numb>
+
+# ask the user to input 2 numbers
+# check if the second number is not equal to zero
+# num_1 / num_2
+# print the quotient with decimal point
