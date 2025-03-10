3be97bd06aab0ea66db3df7df57e57d97464aad2
AceFVA <acefrancisagustin@gmail.com>
2025-03-10 17:21:10 +0800

    Created the main code

--- simple_projects_in_python_1/program_for_adding_two_numbers.py
+++ simple_projects_in_python_1/program_for_adding_two_numbers.py
@@ -1,5 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 # ask the user to input 2 numbers
+num_1 = int(input("Input your first number: "))
+num_2 = int(input("Input your second number: "))
+
 # num_1 + num_2 = "sum"
 # print the sum
+print("Result:", num_1 + num_2)
