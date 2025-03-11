4e49a8af9077e146512b3133a68952588025edd2
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 13:15:37 +0800

    Added pseudocode

---
+++ simple_projects_in_python_2/program_for_printing_smaller_numbers.py
@@ -0,0 +1,7 @@
+# Create a program that ask user to input 2 numbers. Print the smaller number.
+
+# ask the user to input 2 numbers
+# if num_1 is smaller than num_2:
+# print num_1
+# if num_2 is smaller than num_1:
+# print num_2
