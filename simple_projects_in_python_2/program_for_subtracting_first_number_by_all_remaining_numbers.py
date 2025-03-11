3f65321437afe0ccdffedc9a567ea8ab2a649269
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 14:36:21 +0800

    Added a pseudocode

---
+++ simple_projects_in_python_2/program_for_subtracting_first_number_by_all_remaining_numbers.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+# ask the user to input 10 numbers with spaces in between
+# create a list of numbers entered by the user
+# set a variable as a container for the first number
+# create a for loop and iterate all items from the list
+# difference = first number - second-last number
+# print the difference
