84daded90d802504a42c2f64577a4936f863303c
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 13:18:44 +0800

    Added an if-else statement

--- simple_projects_in_python_2/program_for_printing_smaller_numbers.py
+++ simple_projects_in_python_2/program_for_printing_smaller_numbers.py
@@ -4,9 +4,10 @@
 num_1 = int(input("Input your first number: ")
 num_2 = int(input("Input your second number: ")
 # if num_1 is smaller than num_2:
-
+if num_1 < num_2:
 # print num_1
-
+    print(num_1)
 # if num_2 is smaller than num_1:
-
+else:
 # print num_2
+    print(num_2)
