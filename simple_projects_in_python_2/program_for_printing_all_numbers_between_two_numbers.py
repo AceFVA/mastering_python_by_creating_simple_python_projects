e416e678ff13b6d92845037b737c3f18f7964d0d
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 15:55:11 +0800

    Added two input variables

--- simple_projects_in_python_2/program_for_printing_all_numbers_between_two_numbers.py
+++ simple_projects_in_python_2/program_for_printing_all_numbers_between_two_numbers.py
@@ -1,9 +1,15 @@
 # Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 # ask the user to input 2 numbers
-# if num_1 is less than num_2,
+num_1 = int(input("Input your first number: "))
+num_2 = int(input("Input your second number: "))
+
 # num_1 = num_1 + 1, step = 1
+
 # if num_1 is greater than num_2,
+
 # num_1 = num_1 - 1, step = -1
+
 # iterate an item in range(num_1, num_2, step)
+
 # print the number
