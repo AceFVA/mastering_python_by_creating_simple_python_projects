9be7b73eb5b7cf089b579bd88a6161fd03e9c47c
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 01:15:23 +0800

    Modified the pseudocode and the for loop

--- simple_projects_in_python_3/program_for_printing_unique_numbers_only.py
+++ simple_projects_in_python_3/program_for_printing_unique_numbers_only.py
@@ -1,7 +1,7 @@
 # Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
-# ask the user to input 10 numbers 
-numbers = input("Enter 10 numbers with spaces  in between: ")    # example "1 1 2 3 4 5 6 7 8 8"
+# ask the user to input 10 numbers
+numbers = input("Enter 10 numbers with spaces  in between: ")    #example "1 1 2 3 4 5 6 7 8 8"
 
 # create a list for the numbers given by the user
 num_list = numbers.split()
@@ -11,13 +11,10 @@ unique_num = []
 
 # iterate an item in range(10)
 for i in range(10):
-# if a number is not in the list,
-    if num_list[i] not in unique_num:
+# if the count of a number is less than 2,
+    if num_list.count(num_list[i]) < 2:
 # add the number to that list
         unique_num.append(num_list[i])
-# if a number is a duplicate
-    else:
-# skip
-        pass
+        
 # print the unique numbers
-print("The unique numbers are: ", unique_num)
+print("The unique numbers are:", " ".join(unique_num))
