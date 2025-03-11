17e22ac3451fd1a77d0b14ba9c546c6e8aa98127
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 21:55:03 +0800

    Added a variable for list of numbers entered and an empty list

--- simple_projects_in_python_3/program_for_printing_unique_numbers.py
+++ simple_projects_in_python_3/program_for_printing_unique_numbers.py
@@ -5,7 +5,11 @@
 numbers = input("Input 10 numbers separated by space: ")    # example "1 2 3 4 5 6 7 1 2 3"
 
 # create a list for the numbers given by the user
+num_list = numbers.split()
+
 # set a variable with an empy list as a container for unique numbers
+unique_num = []
+
 # iterate an item in range(10) using for loop
 # if number not in the unique list, 
 # add number to that list
