1b19c4d98cb295ce9e39974d1665af2c3ad67d48
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 16:45:40 +0800

    Edited pseudocode, added displaying message

--- simple_projects_in_python_2/program_for_subtracting_first_number_by_all_remaining_numbers.py
+++ simple_projects_in_python_2/program_for_subtracting_first_number_by_all_remaining_numbers.py
@@ -1,8 +1,7 @@
-# Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+# Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
 
 # ask the user to input 10 numbers with spaces in between
-numbers = input("Enter 10 numbers with spaces in between: ")
-# example: "100 9 8 7 6 5 4 3 2 1"
+numbers = input("Enter 10 numbers with spaces in between: ")    # example: "100 9 8 7 6 5 4 3 2 1"
 
 # create a list of numbers entered by the user
 num_list = numbers.split()
@@ -15,4 +14,4 @@ for i in range(1,10):
 # difference = first number - second-last number
     first_num = int(first_num) - int(num_list[i])
 # print the difference
-print(first_num)
+print("Result: ", first_num)
