3e87506fb4a93143f549d8373cae6a40d4b16dac
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 00:56:56 +0800

    Added a pseudocode

---
+++ simple_projects_in_python_3/program_for_printing_unique_numbers_only.py
@@ -0,0 +1,11 @@
+# Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
+
+# ask the user to input 10 numbers 
+# create a list for the numbers given by the user
+# set a variable with an empy list
+# iterate an item in range(10)
+# if a number is not in the list,
+# add the number to that list
+# if a number is a duplicate
+# skip
+# print the number
