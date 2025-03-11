ac222b979c7395ce0125500e8c043159049117c1
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 16:00:36 +0800

    Created a for loop with an initialize value for step

--- simple_projects_in_python_2/program_for_printing_all_numbers_between_two_numbers.py
+++ simple_projects_in_python_2/program_for_printing_all_numbers_between_two_numbers.py
@@ -4,12 +4,14 @@
 num_1 = int(input("Input your first number: "))
 num_2 = int(input("Input your second number: "))
 
+# initialize a step value
+step = 1
+
+# if num_1 is less than num_2,
 # num_1 = num_1 + 1, step = 1
-
 # if num_1 is greater than num_2,
-
 # num_1 = num_1 - 1, step = -1
-
 # iterate an item in range(num_1, num_2, step)
-
+for i in range(num_1, num_2, step):
 # print the number
+   print(i)
