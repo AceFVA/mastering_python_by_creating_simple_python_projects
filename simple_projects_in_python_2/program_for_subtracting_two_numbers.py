bbd84d880512786eb509e8e27ffcf463f75deda7
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 13:40:22 +0800

    Added num_1 and num_2 with input()

--- simple_projects_in_python_2/program_for_subtracting_two_numbers.py
+++ simple_projects_in_python_2/program_for_subtracting_two_numbers.py
@@ -1,5 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
 # ask the user to input 2 numbers
+num_1 = int(input("Input your first number: "))
+num_2 = int(input("Input your second number: "))
+
 # difference = num_1 - num_2
+
 # print the difference
