3ed2729faf1988fcfc0f94f5699a93ed664ead08
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 15:09:16 +0800

    created a list and set a variable with an initialized value of 0

--- simple_projects_in_python_2/program_for_counting_even_numbers.py
+++ simple_projects_in_python_2/program_for_counting_even_numbers.py
@@ -5,8 +5,10 @@ numbers = input("Enter 10 numbers w/ spaces in between: ")
 # example: "1 2 3 4 5 6 7 8 9 10"
 
 # create a list of numbers entered by the user
+num_list = numbers.split()
 
 # set a variable with an initialized value of 0 as a container for the total number of evens
+evens = 0
 
 # create a for loop and iterate an item on the given list
 
