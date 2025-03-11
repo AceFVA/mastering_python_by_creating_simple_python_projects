69f8ed9c31a267b9dde843cf8844fd2635fb5a11
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 13:58:07 +0800

    Added a while loop for checking zero as input in second number

--- simple_projects_in_python_2/program_for_dividing_two_numbers_without_decimal_point.py
+++ simple_projects_in_python_2/program_for_dividing_two_numbers_without_decimal_point.py
@@ -5,5 +5,9 @@ num_1 = int(input("Input your first number: "))
 num_2 = int(input("Input your second number: "))
 
 # check if the second number is not equal to zero
+while num_2 == 0:
+    print("Cannot divide by zero.")
+    num_2 = float(input("Input your second number: "))
+
 # num_1 / num_2
 # print the quotient without decimal point
