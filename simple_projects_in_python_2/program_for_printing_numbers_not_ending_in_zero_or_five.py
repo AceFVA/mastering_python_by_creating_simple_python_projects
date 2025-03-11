fd801ded26da8fc6fdf406cc86b7fd6b1ab28241
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 15:38:36 +0800

    Added a pseudocode

---
+++ simple_projects_in_python_2/program_for_printing_numbers_not_ending_in_zero_or_five.py
@@ -0,0 +1,5 @@
+# Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
+
+# iterate a number from 0 to 100 using for loop
+# if num is not ending with 0 or not ending with 5, print num
+# print num
