2bec794b6218bc7fc78098d014ae8f3a266e5560
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 21:48:48 +0800

    Added an input variable

--- simple_projects_in_python_3/program_for_printing_unique_numbers.py
+++ simple_projects_in_python_3/program_for_printing_unique_numbers.py
@@ -2,6 +2,8 @@
 # Display all numbers. For numbers with duplicate, display only the first entry.
 
 # ask the user to input 10 numbers with spaces in between
+numbers = input("Input 10 numbers separated by space: ")    # example "1 2 3 4 5 6 7 1 2 3"
+
 # create a list for the numbers given by the user
 # set a variable with an empy list as a container for unique numbers
 # iterate an item in range(10) using for loop
