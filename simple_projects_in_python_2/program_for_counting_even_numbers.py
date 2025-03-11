4edf993d2c354b6dda41fb65e3de94875cbaa5d1
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 15:06:26 +0800

    Added a speudocode

---
+++ simple_projects_in_python_2/program_for_counting_even_numbers.py
@@ -0,0 +1,9 @@
+# Create a program that ask user to input 10 numbers. Print how many are even numbers.
+
+# ask the user to input 10 numbers with spaces in between
+# create a list of numbers entered by the user
+# set a variable with an initialized value of 0 as a container for the total number of evens
+# create a for loop and iterate an item on the given list
+# if the remainder of the number divided by 2 is equal to 0, then that number is even
+# even = even + 1
+# print the total number of even
