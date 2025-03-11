56e38f773fec96fc2b9a85066dfa6b7723941276
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 15:25:35 +0800

    Added pseudocode

---
+++ simple_projects_in_python_2/program_for_printing_odd_numbers_from_0_to_100.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+
+# set a variable to an initial value of 0
+# while num is less than or equal to 100, enter loop
+# if num % 2 is not equal to 0, then the number is odd
+# print num
+# num = num + 1
+# when the number is even, num = num +1
