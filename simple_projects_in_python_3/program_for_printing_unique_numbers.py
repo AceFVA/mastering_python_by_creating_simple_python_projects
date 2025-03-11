72a1401f41a307ce4569af256411beea2f902459
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 21:44:16 +0800

    Added a pseudocode

---
+++ simple_projects_in_python_3/program_for_printing_unique_numbers.py
@@ -0,0 +1,10 @@
+# Create a program that ask user to input 10 numbers.
+# Display all numbers. For numbers with duplicate, display only the first entry.
+
+# ask the user to input 10 numbers with spaces in between
+# create a list for the numbers given by the user
+# set a variable with an empy list as a container for unique numbers
+# iterate an item in range(10) using for loop
+# if number not in the unique list, 
+# add number to that list
+# print the number
