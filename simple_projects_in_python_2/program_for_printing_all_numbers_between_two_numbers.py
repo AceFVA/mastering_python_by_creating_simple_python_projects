a3955ad9ced8622878e0e41650e798a6314bdde2
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 15:53:09 +0800

    Added pseudocode

---
+++ simple_projects_in_python_2/program_for_printing_all_numbers_between_two_numbers.py
@@ -0,0 +1,9 @@
+# Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+# ask the user to input 2 numbers
+# if num_1 is less than num_2,
+# num_1 = num_1 + 1, step = 1
+# if num_1 is greater than num_2,
+# num_1 = num_1 - 1, step = -1
+# iterate an item in range(num_1, num_2, step)
+# print the number
