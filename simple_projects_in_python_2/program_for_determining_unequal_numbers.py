a78bfedbb7fe653810a40a89b53e239bf1b81736
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 16:33:01 +0800

    Updated the display message

--- simple_projects_in_python_2/program_for_determining_unequal_numbers.py
+++ simple_projects_in_python_2/program_for_determining_unequal_numbers.py
@@ -3,7 +3,8 @@
 # ask the user to input 2 numbers
 num_1 = int(input("Input your first number: ")
 num_2 = int(input("Input your second number: ")
+
 # if num_1 is not equal to num_2:
 if num_1 != num_2:
 # print "Not Equal"
-    print("Not equal.")
+    print("The numbers are not equal.")
