f441e8276fccfcf5682f6a308195e703ee6f9dc0
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 15:43:02 +0800

    Created for loop that prints all number from 0 to 100 that doesn't ends with 0 or 5

--- simple_projects_in_python_2/program_for_printing_numbers_not_ending_in_zero_or_five.py
+++ simple_projects_in_python_2/program_for_printing_numbers_not_ending_in_zero_or_five.py
@@ -1,5 +1,8 @@
 # Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero or ending five.
 
 # iterate a number from 0 to 100 using for loop
+for i in range(0,101):
 # if num is not ending with 0 or not ending with 5, print num
+    if not(str(i).endswith("0") or str(i).endswith("5")):
 # print num
+        print(i)
