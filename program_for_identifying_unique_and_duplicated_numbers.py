505cf4892058bcfa222e40c169339fab1465cb67
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 00:31:17 +0800

    Added pseudocode

---
+++ program_for_identifying_unique_and_duplicated_numbers.py
@@ -0,0 +1,9 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. 
+# Display "Unique" after input when the inputted number don't have duplicate. 
+# Display "Duplicate" after input when the inputted number have duplicate.
+
+# ask the user to input a number
+# if the number doesn't have a duplicate,
+# print "Unique"
+# if the number does have a duplicate,
+# print "Duplicate"
