5edf236e4805e479dd65281004f385ba45d11f5f
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 02:25:38 +0800

    Added pseudocode

---
+++ simple_projects_in_python_3/program_for_printing_lowest_numbers.py
@@ -0,0 +1,13 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. 
+# Display the lowest number
+
+# create a variable with an empty list
+# ask the user to input an inital number
+# insert the initial number to the list
+# ask the user to input a number continuously until input is invalid
+# if num is less than the number from the list at index 0,
+# print num as the lowest number
+# insert num to the list at index 0
+# if the number from the list at index 0 is less than num,
+# print the number 
+# insert num to the list at index 1
