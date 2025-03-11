efd65c7fdb6c3069c1bd983ba7db7b062f85f474
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 01:00:08 +0800

    added an input variable

--- simple_projects_in_python_3/program_for_printing_unique_numbers_only.py
+++ simple_projects_in_python_3/program_for_printing_unique_numbers_only.py
@@ -1,11 +1,20 @@
 # Create a program that ask user to input 10 numbers. Display all numbers that don't have duplicate.
 
 # ask the user to input 10 numbers 
+numbers = input("Enter 10 numbers with spaces  in between: ")    # example "1 1 2 3 4 5 6 7 8 8"
+
 # create a list for the numbers given by the user
+
 # set a variable with an empy list
+
 # iterate an item in range(10)
+
 # if a number is not in the list,
+
 # add the number to that list
+
 # if a number is a duplicate
+
 # skip
+
 # print the number
