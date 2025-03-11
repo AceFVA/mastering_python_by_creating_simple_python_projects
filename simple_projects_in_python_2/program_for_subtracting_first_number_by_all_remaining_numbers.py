58b6c74cc420a709adfce8acd2f12ef67ed07c45
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 14:39:19 +0800

    Added an input variable

--- simple_projects_in_python_2/program_for_subtracting_first_number_by_all_remaining_numbers.py
+++ simple_projects_in_python_2/program_for_subtracting_first_number_by_all_remaining_numbers.py
@@ -1,8 +1,15 @@
 # Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 # ask the user to input 10 numbers with spaces in between
+numbers = input("Enter 10 numbers with spaces in between: ")
+# example: "100 9 8 7 6 5 4 3 2 1"
+
 # create a list of numbers entered by the user
+
 # set a variable as a container for the first number
+
 # create a for loop and iterate all items from the list
+
 # difference = first number - second-last number
+
 # print the difference
