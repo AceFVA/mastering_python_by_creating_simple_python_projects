c76e350d6d741ff869082d389262f51bd5a9c7df
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 13:30:22 +0800

    Added an if statement

--- simple_projects_in_python_2/program_for_determining_unequal_numbers.py
+++ simple_projects_in_python_2/program_for_determining_unequal_numbers.py
@@ -4,5 +4,6 @@
 num_1 = int(input("Input your first number: ")
 num_2 = int(input("Input your second number: ")
 # if num_1 is not equal to num_2:
-
+if num_1 != num_2:
 # print "Not Equal"
+    print("Not equal.")
