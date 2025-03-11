9de2e79d26502a63f00d1deabb29348994143ba9
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 02:36:28 +0800

    Added the if-else statement inside while loop

--- simple_projects_in_python_3/program_for_printing_lowest_numbers.py
+++ simple_projects_in_python_3/program_for_printing_lowest_numbers.py
@@ -15,9 +15,14 @@ num_list.insert(0, num)
 while True:
     num = int(input("Enter a number: "))
 # if num is less than the number from the list at index 0,
-    
+    if num < num_list[0]:
 # print num as the lowest number
+        print("The lowest number is: ", num)
 # insert num to the list at index 0
+        num_list.insert(0, num)
 # if the number from the list at index 0 is less than num,
-# print the number 
+    else:
+# print the number
+        print("The lowest number is: ", num_list[0])
 # insert num to the list at index 1
+        num_list.insert(1, num)
