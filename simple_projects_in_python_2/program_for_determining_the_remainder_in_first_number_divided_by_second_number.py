b209615dd7ab0d7cbedbe67492c30ede8d92fc27
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 16:48:54 +0800

    Edited pseudocode

--- simple_projects_in_python_2/program_for_determining_the_remainder_in_first_number_divided_by_second_number.py
+++ simple_projects_in_python_2/program_for_determining_the_remainder_in_first_number_divided_by_second_number.py
@@ -1,4 +1,4 @@
-# Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+# Create a program that ask user to input 2 numbers. Print the remainder when the first number is divided by the second number.
  
 # ask the user to input 2 numbers
 num_1 = int(input("Input your first number: "))
