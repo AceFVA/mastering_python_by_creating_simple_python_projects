fe2ca4134a23033643f51619048b3f9f0d071ab1
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 13:54:23 +0800

    Added two variables with input()

--- simple_projects_in_python_2/program_for_dividing_two_numbers_without_decimal_point.py
+++ simple_projects_in_python_2/program_for_dividing_two_numbers_without_decimal_point.py
@@ -1,6 +1,9 @@
 # Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
  
 # ask the user to input 2 numbers
+num_1 = int(input("Input your first number: "))
+num_2 = int(input("Input your second number: "))
+
 # check if the second number is not equal to zero
 # num_1 / num_2
 # print the quotient without decimal point
