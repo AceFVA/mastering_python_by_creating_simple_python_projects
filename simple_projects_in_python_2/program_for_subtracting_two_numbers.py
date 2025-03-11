3241a9e8899dd5ec640181577efd57a924f54a06
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 13:36:30 +0800

    Added a pseudocode

---
+++ simple_projects_in_python_2/program_for_subtracting_two_numbers.py
@@ -0,0 +1,5 @@
+# Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
+
+# ask the user to input 2 numbers
+# difference = num_1 - num_2
+# print the difference
