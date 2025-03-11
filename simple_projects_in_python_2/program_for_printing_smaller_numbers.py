86a63f5e373e1c83667b54b2725e69688bfdfd22
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 13:17:27 +0800

    Added num_1 and num_2 varible

--- simple_projects_in_python_2/program_for_printing_smaller_numbers.py
+++ simple_projects_in_python_2/program_for_printing_smaller_numbers.py
@@ -1,7 +1,12 @@
 # Create a program that ask user to input 2 numbers. Print the smaller number.
 
 # ask the user to input 2 numbers
+num_1 = int(input("Input your first number: ")
+num_2 = int(input("Input your second number: ")
 # if num_1 is smaller than num_2:
+
 # print num_1
+
 # if num_2 is smaller than num_1:
+
 # print num_2
