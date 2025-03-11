7c09d08b8bd739484c57f53201b5e3f873d3dec1
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 15:07:47 +0800

    Added an input variable

--- simple_projects_in_python_2/program_for_counting_even_numbers.py
+++ simple_projects_in_python_2/program_for_counting_even_numbers.py
@@ -1,9 +1,17 @@
 # Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
 # ask the user to input 10 numbers with spaces in between
+numbers = input("Enter 10 numbers w/ spaces in between: ") 
+# example: "1 2 3 4 5 6 7 8 9 10"
+
 # create a list of numbers entered by the user
+
 # set a variable with an initialized value of 0 as a container for the total number of evens
+
 # create a for loop and iterate an item on the given list
+
 # if the remainder of the number divided by 2 is equal to 0, then that number is even
+
 # even = even + 1
+
 # print the total number of even
