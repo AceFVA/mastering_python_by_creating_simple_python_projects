042c46f4df3546cf6f059bad10ba733998f390c8
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 17:01:45 +0800

    Adjusted a single line comment

--- simple_projects_in_python_1/program_for_summation_of_10_numbers.py
+++ simple_projects_in_python_1/program_for_summation_of_10_numbers.py
@@ -1,8 +1,7 @@
 # Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 # ask the user to input 10 numbers with spaces in between
-numbers = input("Enter 10 numbers with spaces in between: ")
-# example: "1 2 3 4 5 6 7 8 9 10"
+numbers = input("Enter 10 numbers with spaces in between: ")    # example: "1 2 3 4 5 6 7 8 9 10"
 
 # create a list of numbers entered by the user
 num_list = numbers.split()
