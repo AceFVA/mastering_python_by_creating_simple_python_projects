dae5fa99cc73071ba6ddecaf920df231f49e830a
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 16:29:13 +0800

    Added an prompting message

--- simple_projects_in_python_2/program_for_counting_even_numbers.py
+++ simple_projects_in_python_2/program_for_counting_even_numbers.py
@@ -1,8 +1,7 @@
 # Create a program that ask user to input 10 numbers. Print how many are even numbers.
 
 # ask the user to input 10 numbers with spaces in between
-numbers = input("Enter 10 numbers w/ spaces in between: ") 
-# example: "1 2 3 4 5 6 7 8 9 10"
+numbers = input("Enter 10 numbers w/ spaces in between: ")    # example: "1 2 3 4 5 6 7 8 9 10"
 
 # create a list of numbers entered by the user
 num_list = numbers.split()
@@ -16,5 +15,6 @@ for i in range(10):
     if int(num_list[i]) % 2 == 0:
 # evens = evens + 1
         evens += 1
+
 # print the total number of even
-print(evens)
+print("The total no. of even is: ", evens)
