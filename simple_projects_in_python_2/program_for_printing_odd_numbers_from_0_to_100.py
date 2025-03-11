19dc1ebc71068a314f48676ffb17e14d91daf8b4
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 15:28:43 +0800

    Added a while loop

--- simple_projects_in_python_2/program_for_printing_odd_numbers_from_0_to_100.py
+++ simple_projects_in_python_2/program_for_printing_odd_numbers_from_0_to_100.py
@@ -1,8 +1,15 @@
 # Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
 
 # set a variable to an initial value of 0
+num = 0
+
 # while num is less than or equal to 100, enter loop
+while num <= 100:
 # if num % 2 is not equal to 0, then the number is odd
+    if num % 2 != 0:
 # print num
+        print(num)
 # num = num + 1
+        num += 1
+
 # when the number is even, num = num +1
