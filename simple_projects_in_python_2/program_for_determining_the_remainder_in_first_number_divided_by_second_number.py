4213bb99ea74df828be79f4340c9b1cd22eb894b
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 14:16:28 +0800

    Added num_1 and num_2 with input()

--- simple_projects_in_python_2/program_for_determining_the_remainder_in_first_number_divided_by_second_number.py
+++ simple_projects_in_python_2/program_for_determining_the_remainder_in_first_number_divided_by_second_number.py
@@ -1,5 +1,10 @@
 # Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
  
 # ask the user to input 2 numbers
+num_1 = int(input("Input your first number: "))
+num_2 = int(input("Input your second number: "))
+
 # remainder = num_1 % num_2
+
+
 # print remainder
