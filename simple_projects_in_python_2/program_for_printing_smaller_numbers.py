557cf2d9d04997360cd3d2814a7ee8027fa71aa4
AceFVA <acefrancisagustin@gmail.com>
2025-03-11 16:42:54 +0800

    Fixed the SyntaxError

--- simple_projects_in_python_2/program_for_printing_smaller_numbers.py
+++ simple_projects_in_python_2/program_for_printing_smaller_numbers.py
@@ -1,8 +1,8 @@
 # Create a program that ask user to input 2 numbers. Print the smaller number.
 
 # ask the user to input 2 numbers
-num_1 = int(input("Input your first number: ")
-num_2 = int(input("Input your second number: ")
+num_1 = int(input("Input your first number: "))
+num_2 = int(input("Input your second number: "))
 # if num_1 is smaller than num_2:
 if num_1 < num_2:
 # print num_1
