5ce59a5479aa9a145c56471b7398d75eaa48544b
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 17:17:18 +0800

    added an initial pseudocode

---
+++ simple_projects_in_python_4/program_for_calculating_average_of_inputted_numbers.py
@@ -0,0 +1,9 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. 
+# qDisplay the average.
+
+# create an empty list as container for the numbers to be inputted
+# ask the user to input a number continuously until input is invalid
+# add the number inputted on the list
+# summation = initial number + succeeding numbers
+# average = summation / number of all items on the list
+# print average
