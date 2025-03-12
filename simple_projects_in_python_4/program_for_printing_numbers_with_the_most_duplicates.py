d6f7d39b34f19a411b6108b6800fab21567de396
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 18:31:11 +0800

    Added an unfinished while loop

--- simple_projects_in_python_4/program_for_printing_numbers_with_the_most_duplicates.py
+++ simple_projects_in_python_4/program_for_printing_numbers_with_the_most_duplicates.py
@@ -2,12 +2,24 @@
 # Display the number with the most number of duplicate.
 
 # create an empty list
+num_list = []
+
 # ask the user to input a number continuously until the input is invalid
+while True:
+    input_num = int(input("Enter a number: "))
 # add the inputted number on the list
+    num_list.append(input_num)
 # create most_duplicated and most_count for most duplicated and most counted number with initial value of 0
+    most_duplicated = 0
+    most_count = 0
 # iterate the number from the list
+
 # count = count of inputted number on the list
+
 # if count is greater than most_count,
+
 # most_duplicated = inputted number
+
 # most_count = count
+
 # print the most duplicated number
