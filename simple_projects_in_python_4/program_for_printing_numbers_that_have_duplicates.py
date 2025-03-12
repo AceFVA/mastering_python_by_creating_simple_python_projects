e4ffad305ad1d6dedd13cc2f21e54434f7ad9e46
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 15:07:30 +0800

    Added pseudocode

---
+++ simple_projects_in_python_4/program_for_printing_numbers_that_have_duplicates.py
@@ -0,0 +1,9 @@
+# Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
+
+# ask the user to input 10 numbers with spaces in between
+# create a list for the numbers inputted
+# create a for loop to iterate an item from the list
+# verify if a number have duplicate
+# print the numbers with duplicate
+
+
