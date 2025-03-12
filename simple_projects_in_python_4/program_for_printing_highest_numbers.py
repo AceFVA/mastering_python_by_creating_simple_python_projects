15b14292af34d68c1d3f448fd01b9f706bcacd1a
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 16:47:05 +0800

    Added pseudocode

---
+++ simple_projects_in_python_4/program_for_printing_highest_numbers.py
@@ -0,0 +1,13 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. 
+# Display the highest number
+
+# create a variable with an empty list
+# ask the user to input an initial number
+# insert the initial number to the list
+# ask the user to input a number continuously until input is invalid
+# if num is more than the number from the list at index 0,
+# print num as the highest number
+# insert num to the list at index 0
+# if the number from the list at index 0 is more than num,
+# print the number
+# insert num to the list at index 1
