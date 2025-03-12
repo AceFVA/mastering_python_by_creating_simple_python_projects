a35fbbc360eae77a5f798e64e8cdbff0e4f718d9
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 18:28:44 +0800

    Added a pseudocode

---
+++ simple_projects_in_python_4/program_for_printing_numbers_with_the_most_duplicates.py
@@ -0,0 +1,13 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid.
+# Display the number with the most number of duplicate.
+
+# create an empty list
+# ask the user to input a number continuously until the input is invalid
+# add the inputted number on the list
+# create most_duplicated and most_count for most duplicated and most counted number with initial value of 0
+# iterate the number from the list
+# count = count of inputted number on the list
+# if count is greater than most_count,
+# most_duplicated = inputted number
+# most_count = count
+# print the most duplicated number
