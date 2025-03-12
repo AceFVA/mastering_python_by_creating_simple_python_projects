d8f7065c29aec1b73fbeb84ebb8e7118bb9224f8
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 16:56:18 +0800

    created a while loop as the main code

--- simple_projects_in_python_4/program_for_printing_highest_numbers.py
+++ simple_projects_in_python_4/program_for_printing_highest_numbers.py
@@ -4,22 +4,25 @@
 # create a variable with an empty list
 num_list = []
 # ask the user to input an initial number
-num = int(input("Enter an number: "))
+num = int(input("Enter a number: "))
 print ("The highest number is: ", num)
 
 # insert the initial number to the list
 num_list.insert(0,num)
 
 # ask the user to input a number continuously until input is invalid
-
+while True:
+    num = int(input("Enter a number: "))
 # if num is more than the number from the list at index 0,
-
+    if num > num_list[0]:
 # print num as the highest number
-
+        print("The highest number is: ", num)
 # insert num to the list at index 0
+        num_list.insert(0,num)
 
 # if the number from the list at index 0 is more than num,
-
+    else:
 # print the number
-
+        print("The highest number is: ", num_list[0])
 # insert num to the list at index 1
+        num_list.insert(1,num)
