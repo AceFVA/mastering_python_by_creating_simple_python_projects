bf3851c22daa736ca44a43c3c1240a0e91adf4fd
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 17:43:36 +0800

    Variable name changed to a descriptive one

--- simple_projects_in_python_4/program_for_printing_highest_numbers.py
+++ simple_projects_in_python_4/program_for_printing_highest_numbers.py
@@ -4,25 +4,25 @@
 # create a variable with an empty list
 num_list = []
 # ask the user to input an initial number
-num = int(input("Enter a number: "))
-print ("The highest number is: ", num)
+input_num = int(input("Enter a number: "))
+print ("The highest number is: ", input_num)
 
 # insert the initial number to the list
-num_list.insert(0,num)
+num_list.insert(0, input_num)
 
 # ask the user to input a number continuously until input is invalid
 while True:
-    num = int(input("Enter a number: "))
+    input_num = int(input("Enter a number: "))
 # if num is more than the number from the list at index 0,
-    if num > num_list[0]:
+    if input_num > num_list[0]:
 # print num as the highest number
-        print("The highest number is: ", num)
+        print("The highest number is: ", input_num)
 # insert num to the list at index 0
-        num_list.insert(0,num)
+        num_list.insert(0,input_num)
 
 # if the number from the list at index 0 is more than num,
     else:
 # print the number
         print("The highest number is: ", num_list[0])
 # insert num to the list at index 1
-        num_list.insert(1,num)
+        num_list.insert(1,input_num)
