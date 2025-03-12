0508e6f0ae69012e1d57b56b8de5fc50c6ed494d
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 16:49:38 +0800

    Added initial codes

--- simple_projects_in_python_4/program_for_printing_highest_numbers.py
+++ simple_projects_in_python_4/program_for_printing_highest_numbers.py
@@ -2,12 +2,24 @@
 # Display the highest number
 
 # create a variable with an empty list
+num_list = []
 # ask the user to input an initial number
+num = int(input("Enter an number: "))
+print ("The highest number is: ", num)
+
 # insert the initial number to the list
+num_list.insert(0,num)
+
 # ask the user to input a number continuously until input is invalid
+
 # if num is more than the number from the list at index 0,
+
 # print num as the highest number
+
 # insert num to the list at index 0
+
 # if the number from the list at index 0 is more than num,
+
 # print the number
+
 # insert num to the list at index 1
