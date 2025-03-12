a17c20de4826610c7791c5a0eb177278e038eef3
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 17:03:32 +0800

    Added pseudocode

---
+++ simple_projects_in_python_4/program_for_sorting_numbers_in_descending_order.py
@@ -0,0 +1,8 @@
+# Create a program that ask user to input a number, continue asking until the user input is invalid. 
+# Display the number from highest to lowest. Clue: sort() function
+
+# create an empty list as container for the numbers to be inputted
+# ask the user to input a number continuously until input is invalid
+# add the number inputted on the list
+# sort the numbers in descending order
+# print the numbers
