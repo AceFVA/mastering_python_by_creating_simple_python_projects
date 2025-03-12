cd5ed6d5cb04dcd8ad610d4e91d0cee240d5cff4
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 17:34:13 +0800

    Added variables

--- simple_projects_in_python_4/program_for_calculating_average_of_inputted_numbers.py
+++ simple_projects_in_python_4/program_for_calculating_average_of_inputted_numbers.py
@@ -2,9 +2,18 @@
 # Display the average.
 
 # create an empty list as container for the numbers to be inputted
+num_list = []
+
 # set 2 variables with initialize value of 0 for summation and average
+summation = 0
+average = 0
+ 
 # ask the user to input a number continuously until input is invalid
+
 # add the number inputted on the list
+
 # summation = summation + input_num
+
 # average = summation / number of all items on the list
+
 # print average
