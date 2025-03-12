ec892e031a55c9108c3317719ade95c49edd70ac
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 15:14:42 +0800

    Added an input variable and an empty list

--- simple_projects_in_python_4/program_for_printing_numbers_that_have_duplicates.py
+++ simple_projects_in_python_4/program_for_printing_numbers_that_have_duplicates.py
@@ -1,7 +1,11 @@
 # Create a program that ask user to input 10 numbers. Display all numbers that have duplicate.
 
 # ask the user to input 10 numbers with spaces in between
+numbers = input("Enter 10 numbers with spaces in between: ")    # example "1 2 3 4 2 3 4 5 8 9"
+
 # create a list for the numbers inputted
+num_list = numbers.split()
+
 # create a for loop to iterate an item from the list
 # verify if a number have duplicate
 # print the numbers with duplicate
