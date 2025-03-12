9977ad37f2dbe7692199357703c22cd41c363167
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 12:05:43 +0800

    Added an empty list and an input variable

--- simple_projects_in_python_3/program_for_sorting_numbers.py
+++ simple_projects_in_python_3/program_for_sorting_numbers.py
@@ -2,7 +2,12 @@
 # Display the number from lowest to highest. Clue: sort() function
 
 # create an empty list as container for the numbers to be inputted
+num_list = []
+
 # ask the user to input a number continuously until input is invalid
+while True:
+    input_num = int(input("Enter a number: "))
+
 # add the number inputted on the list
 # sort the numbers in ascending order
 # print the numbers
