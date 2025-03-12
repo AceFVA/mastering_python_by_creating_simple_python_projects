aeb13257824ef926e96c384ffec179c5ddc78e2f
AceFVA <acefrancisagustin@gmail.com>
2025-03-12 18:36:08 +0800

    Added a for loop inside the while loop for determination of most duplicated number

--- simple_projects_in_python_4/program_for_printing_numbers_with_the_most_duplicates.py
+++ simple_projects_in_python_4/program_for_printing_numbers_with_the_most_duplicates.py
@@ -13,13 +13,14 @@ while True:
     most_duplicated = 0
     most_count = 0
 # iterate the number from the list
-
+    for input_num in num_list:
 # count = count of inputted number on the list
-
+        count = num_list.count(num)
 # if count is greater than most_count,
-
+        if count > most_count:
 # most_duplicated = inputted number
-
 # most_count = count
-
+           most_duplicated = input_num
+           most_count = count
 # print the most duplicated number
+    print("The most duplicated number is: ", most_duplicated)
