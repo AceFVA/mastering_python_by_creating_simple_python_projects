fc65c9e28fb8e308813c40c807c8b5cde254aed7
AceFVA <acefrancisagustin@gmail.com>
2025-03-19 23:38:11 +0800

    added the lstrip() method

--- simple_projects_in_python_5/program_for_removing_spaces_in_the_beginning.py
+++ simple_projects_in_python_5/program_for_removing_spaces_in_the_beginning.py
@@ -2,5 +2,7 @@
 #Print the input without the spaces in the beginning.
 
 # ask the user to enter his/her fullname with several space characters at the beginning.
+full_name = input("Please enter your full name with spaces at the beginning: ")
 # remove left padded spaces
 # print full name
+print(full_name.lstrip())
