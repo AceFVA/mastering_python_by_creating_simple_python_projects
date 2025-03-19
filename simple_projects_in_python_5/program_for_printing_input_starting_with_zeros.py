34608d4284cdba5575dfed84b6589afa63975a19
AceFVA <acefrancisagustin@gmail.com>
2025-03-19 23:45:22 +0800

    added an intial pseudocode

---
+++ simple_projects_in_python_5/program_for_printing_input_starting_with_zeros.py
@@ -0,0 +1,6 @@
+# Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format.
+# Add zeros at the beginning to complete the 6 digit.
+
+# ask the user to input any number starting from 0 to 1000
+# add zeros at the beginning to make it in a 6-digit format
+# print the digits
