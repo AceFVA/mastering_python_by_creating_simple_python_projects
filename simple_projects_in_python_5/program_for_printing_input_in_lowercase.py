94c4580f9d77b70e05a681284fb924c97f1ff48d
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 00:14:10 +0800

    Added a code with lower() string method

--- simple_projects_in_python_5/program_for_printing_input_in_lowercase.py
+++ simple_projects_in_python_5/program_for_printing_input_in_lowercase.py
@@ -1,5 +1,10 @@
 # Create a program that ask the user to input their fullname. Print the input in all lower case.
 
 # ask the user to input his/her full name
+full_name = input("Enter your full name: ")
+
 # convert full name to lowercase
+in_lowercase = full_name.lower()
+
 # print full name in lowercase
+print(in_lowercase)
