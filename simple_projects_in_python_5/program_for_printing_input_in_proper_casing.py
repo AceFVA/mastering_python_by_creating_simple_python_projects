111afdc761a69fe41c92e99570a4995b4cb91559
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 00:20:01 +0800

    Added the code with title() string method

--- simple_projects_in_python_5/program_for_printing_input_in_proper_casing.py
+++ simple_projects_in_python_5/program_for_printing_input_in_proper_casing.py
@@ -1,5 +1,10 @@
 # Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
 
 # ask the user to input his/her full name
+full_name = input("Enter your full name: ")
+
 # convert input in proper casing 
+in_title_case = full_name.title()
+
 # print full name in proper casing
+print(in_title_case)
