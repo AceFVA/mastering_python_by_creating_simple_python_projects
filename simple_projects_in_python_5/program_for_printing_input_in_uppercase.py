2792d873bc44fa9e9589794cea33a6c08ef98419
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 00:05:37 +0800

    Added pseudocode

---
+++ simple_projects_in_python_5/program_for_printing_input_in_uppercase.py
@@ -0,0 +1,5 @@
+# Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+# ask the user to input his/her full name
+# convert input to uppercase
+# print full name in uppercase
