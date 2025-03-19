3e377d10ae77ed963ab757ca6ead5f236ba75745
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 00:11:59 +0800

    Added a pseudocode

---
+++ simple_projects_in_python_5/program_for_printing_input_in_lowercase.py
@@ -0,0 +1,5 @@
+# Create a program that ask the user to input their fullname. Print the input in all lower case.
+
+# ask the user to input his/her full name
+# convert full name to lowercase
+# print full name in lowercase
