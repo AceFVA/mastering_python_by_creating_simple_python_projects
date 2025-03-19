25b42e2ae88f3c0dd5b5b8d7194f2c42cf90e829
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 00:08:42 +0800

    Created a code with upper() method

--- simple_projects_in_python_5/program_for_printing_input_in_uppercase.py
+++ simple_projects_in_python_5/program_for_printing_input_in_uppercase.py
@@ -1,5 +1,10 @@
 # Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 # ask the user to input his/her full name
+full_name = input("Enter your full name: ")
+
 # convert input to uppercase
+in_uppercase = full_name.upper()
+
 # print full name in uppercase
+print(in_uppercase)
