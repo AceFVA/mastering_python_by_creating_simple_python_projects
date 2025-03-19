89ae6013750eec58ffcf57a1cf256ddeafa807cc
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 00:17:43 +0800

    added a pseudocode

---
+++ simple_projects_in_python_5/program_for_printing_input_in_proper_casing.py
@@ -0,0 +1,5 @@
+# Create a program that ask the user to input their fullname in incorrect casing. Print the input in proper casing.
+
+# ask the user to input his/her full name
+# convert input in proper casing 
+# print full name in proper casing
