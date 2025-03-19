5be7ece672e1f40798899a75a1c41fe01029b6ce
AceFVA <acefrancisagustin@gmail.com>
2025-03-19 23:34:37 +0800

    added pseudocode

---
+++ simple_projects_in_python_5/program_for_removing_spaces_in_the_beginning.py
@@ -0,0 +1,6 @@
+#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning.
+#Print the input without the spaces in the beginning.
+
+# ask the user to enter his/her fullname with several space characters at the beginning.
+# remove left padded spaces
+# print full name
