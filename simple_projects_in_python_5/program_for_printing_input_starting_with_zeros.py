f67a246224690860f5b8fdcc1b44090c29ff3314
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 00:27:58 +0800

    Added a displaying message

--- simple_projects_in_python_5/program_for_printing_input_starting_with_zeros.py
+++ simple_projects_in_python_5/program_for_printing_input_starting_with_zeros.py
@@ -1,4 +1,4 @@
-# Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format.
+# Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format.
 # Add zeros at the beginning to complete the 6 digit.
 
 # ask the user to input any number starting from 0 to 1000
@@ -8,4 +8,4 @@ input_num = input("Enter a number between 0 and 1000: ")
 with_zeros = input_num.zfill(6)
 
 # print the digits
-print(with_zeros)
+print("Your number in 6-digit format: ", with_zeros)
