079d767e6fb9a40a2f47e28736c6d85f42563000
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 00:29:33 +0800

    Fixed spacing and added displaying message

--- simple_projects_in_python_5/program_for_removing_spaces_in_the_beginning.py
+++ simple_projects_in_python_5/program_for_removing_spaces_in_the_beginning.py
@@ -1,9 +1,11 @@
-#Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning.
-#Print the input without the spaces in the beginning.
+# Create a program that ask the user to input their fullname with several space characters at the beginning.
+# Print the input without the spaces in the beginning.
 
 # ask the user to enter his/her fullname with several space characters at the beginning.
 full_name = input("Please enter your full name with spaces at the beginning: ")
+
 # remove left padded spaces
 removed_spaces = full_name.lstrip()
+
 # print full name
-print(removed_spaces)
+print("Your full name is: ", removed_spaces)
