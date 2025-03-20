2ed9f202726620ab9a8dbac93f847a0a0f4d06b5
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 16:53:07 +0800

    added pseudocode

---
+++ simple_projects_in_python_5/program_for_printing_input_in_pascal_casing.py
@@ -0,0 +1,7 @@
+# Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
+
+# ask the user to input his/her full name
+# convert casing to title case
+# split the input by default
+# join each item to turn it into pascal case
+# print full name in pascal casing
