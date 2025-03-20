156d53a88ae5c2b2360e597ddfc7d2daf7ce999d
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 15:46:38 +0800

    Added initial pseudocode

---
+++ simple_projects_in_python_5/program_for_counting_numbers_of_inputted_words.py
@@ -0,0 +1,5 @@
+# Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+# ask the user to input a statement
+# count the number of words in the statement inputted by the user
+# print the count of words
