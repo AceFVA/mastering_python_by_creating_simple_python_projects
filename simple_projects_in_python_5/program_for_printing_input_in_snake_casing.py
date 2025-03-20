436de58191ce91a37be67bcdf5c161d0c96601f8
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 17:08:32 +0800

    added peudocode

---
+++ simple_projects_in_python_5/program_for_printing_input_in_snake_casing.py
@@ -0,0 +1,6 @@
+# Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
+
+# ask the user to input his/her full name in an incorrect casing
+# convert casing into lower case
+# convert " " to "_"
+# print full name in snake case
