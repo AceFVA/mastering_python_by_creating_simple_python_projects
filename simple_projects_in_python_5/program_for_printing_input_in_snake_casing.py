504424cc88e9b73c5d0bd379d1c68f0bfa311cc4
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 17:11:17 +0800

    Added initial lines of codes with print() for checking output

--- simple_projects_in_python_5/program_for_printing_input_in_snake_casing.py
+++ simple_projects_in_python_5/program_for_printing_input_in_snake_casing.py
@@ -1,6 +1,13 @@
 # Create a program that ask the user to input their fullname in incorrect casing. Print the input in snake case.
 
 # ask the user to input his/her full name in an incorrect casing
+full_name = input("Enter your full name in an incorrect casing: ")
+
 # convert casing into lower case
+in_lower_case = full_name.lower()
+
 # convert " " to "_"
+
+
 # print full name in snake case
+print(in_lower_case)
