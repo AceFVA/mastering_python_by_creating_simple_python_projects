8af539c5ae6a3071ab5646ae17032ed76e50f817
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 16:11:54 +0800

    added a variable for input

--- simple_projects_in_python_5/program_for_counting_numbers_of_characters.py
+++ simple_projects_in_python_5/program_for_counting_numbers_of_characters.py
@@ -1,6 +1,10 @@
 # Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 # ask the user to input his/her full name
+full_name = input("Enter your full name: ")
+
 # count the number of characters
+
+
 # print the numbe of characters in the given full name
 
