0a17249924d8d1fe3c4f67214e6f2d34dd1eb4b6
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 15:32:55 +0800

    Added pseudocode

---
+++ simple_projects_in_python_5/program_for_printing_input_in_reverse_casing.py
@@ -0,0 +1,5 @@
+# Create a program that ask the user to input their fullname in incorrect casing. Print each character of the input in reverse casing.
+
+# ask the user to input his/her full name
+# convert casing to its reversed casing
+# print full name in reversed casing
