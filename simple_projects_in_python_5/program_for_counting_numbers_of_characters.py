66fc94bfe7222bc7ce66fa8c6a9ae5feb86e12f1
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 16:05:58 +0800

    Added pseudocode

---
+++ simple_projects_in_python_5/program_for_counting_numbers_of_characters.py
@@ -0,0 +1,6 @@
+# Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+# ask the user to input his/her full name
+# count the number of characters
+# print the numbe of characters in the given full name
+
