7470708f1a7c9df70b75aa19d1d01b845d76c68c
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 16:58:49 +0800

    Added initial code with print() for checking

--- simple_projects_in_python_5/program_for_printing_input_in_pascal_casing.py
+++ simple_projects_in_python_5/program_for_printing_input_in_pascal_casing.py
@@ -1,7 +1,16 @@
 # Create a program that ask the user to input their fullname in incorrect casing. Print the input in pascal case.
 
 # ask the user to input his/her full name
+full_name = input("Enter your full name: ")
+
 # convert casing to title case
+in_title_case = full_name.title()
+
 # split the input by default
+splitted_input = in_title_case.split()
+
 # join each item to turn it into pascal case
+
+
 # print full name in pascal casing
+print("Your full name in pascal case: ", splitted_input)
