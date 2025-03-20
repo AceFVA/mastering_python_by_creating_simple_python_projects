88b2c1fa202ed193a7f50040ac8085c13c584390
AceFVA <acefrancisagustin@gmail.com>
2025-03-20 15:57:11 +0800

    Created the program usign len() and split() string method

--- simple_projects_in_python_5/program_for_counting_numbers_of_inputted_words.py
+++ simple_projects_in_python_5/program_for_counting_numbers_of_inputted_words.py
@@ -1,5 +1,10 @@
 # Create a program that ask the user to input their fullname. Print the number of characters in the input.
 
 # ask the user to input a statement
+statement = input("Enter a complete statement: ")
+
 # count the number of words in the statement inputted by the user
+counted_words = len(statement.split())
+
 # print the count of words
+print("Number of words: ", counted_words)
