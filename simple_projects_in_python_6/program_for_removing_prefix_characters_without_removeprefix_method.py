5cfdba0cda4e0fd9220c4f4cc7df6df66f2547e6
AceFVA <acefrancisagustin@gmail.com>
2025-03-26 18:27:43 +0800

    Added initial lines of code (incomplete)

--- simple_projects_in_python_6/program_for_removing_prefix_characters_without_removeprefix_method.py
+++ simple_projects_in_python_6/program_for_removing_prefix_characters_without_removeprefix_method.py
@@ -2,8 +2,16 @@
 # Create a program that do the same functionality without using removeprefix() function.
 
 # set a variable with  a string of characters
+string = SB19Ace
+
 # set the prefix to be removed
+prefix = SB19
+
 # create a for loop to iterate all characters in the given input
+
 # remove characters based on the desired prefix
+
 # print the string with removed prefix
+print(string)
+
 
