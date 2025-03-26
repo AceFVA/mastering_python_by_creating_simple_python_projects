500db3579c39a27866db088fc2f7063baad11b41
AceFVA <acefrancisagustin@gmail.com>
2025-03-26 17:50:13 +0800

    Added pseudocode

---
+++ simple_projects_in_python_6/program_for_removing_left_padded_spaces_without_lstrip_method.py
@@ -0,0 +1,7 @@
+# lstrip() remove the space characters at the beginning of the string.
+# Create a program that do the same functionality without using lstrip() function.
+
+# input any characters with left padded spaces
+# initialize a variable at value of 0 as container for left-padded spaces
+# create a for loop that identifies the left-padded spaces
+# print the input without left-padded spaces
