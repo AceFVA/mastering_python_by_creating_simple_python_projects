e8083585db85197ca49577ccfade5d2131fd4ec6
AceFVA <acefrancisagustin@gmail.com>
2025-03-26 17:53:13 +0800

    Added initial lines of codes(incomplete)

--- simple_projects_in_python_6/program_for_removing_left_padded_spaces_without_lstrip_method.py
+++ simple_projects_in_python_6/program_for_removing_left_padded_spaces_without_lstrip_method.py
@@ -2,6 +2,10 @@
 # Create a program that do the same functionality without using lstrip() function.
 
 # input any characters with left padded spaces
+input_str = input("Enter any characters with left-padded spaces: ")
 # initialize a variable at value of 0 as container for left-padded spaces
+left_spaces = 0
 # create a for loop that identifies the left-padded spaces
+
 # print the input without left-padded spaces
+print("Removed left-padded spaces: ")
