3e665f719b5665a3ed5e58465010145e21cb16da
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 00:48:59 +0800

    Added speudocode

---
+++ simple_projects_in_python_6/program_for_checking_characters_if_upper_cased.py
@@ -0,0 +1,14 @@
+# isupper() check if all characters of the string is on upper case.
+# Create a program that do the same functionality without using isupper() function.
+
+# create a variable with string type element
+# create a variable the numbers of uppercased and  non-alphanumeric characters
+# iterate all characters in the given string
+# if a character is not in lowercase,
+# uppercase variable + 1
+# if a character is not alphanumeric,
+# uppercase variable + 1
+# if uppercase variable is equal to the length of the string,
+# print "All characters are in UPPERCASE"
+# if uppercase variable is not equal to the length of the string
+# print "All characters are not in UPPERCASE"
