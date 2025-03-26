adee5125d3daf8c2b98dd704fe8960eae004c0fb
AceFVA <acefrancisagustin@gmail.com>
2025-03-26 19:31:39 +0800

    Added lines of code (initial)

--- program_for_converting_characters_into_lower_case_without_lower_string_method.py
+++ program_for_converting_characters_into_lower_case_without_lower_string_method.py
@@ -1,10 +1,15 @@
 # lower() converts all characters of the string into lower case. Create a program that do the same functionality without using lower() function.
 
 # set a variable with an element of string of characters
+string = "AcE fRaNcIs V. aGuStIn"
+
 # create an empty list for lower cased characters
+lower_cased = []
+
 # create a for loop to iterate all items in the given string
 # if char in string is in upper case,
 # change the case of the char and add to the list
 # if char is not in upper case,
 # add char to the list
 # print the string of characters in lower case
+print(lower_cased)
