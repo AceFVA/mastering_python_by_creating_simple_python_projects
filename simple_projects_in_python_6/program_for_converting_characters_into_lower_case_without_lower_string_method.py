cf25cac34ac85742a8c82e54fb1b930b395deca6
AceFVA <acefrancisagustin@gmail.com>
2025-03-26 19:42:25 +0800

    Moved the source code to the correct file

---
+++ simple_projects_in_python_6/program_for_converting_characters_into_lower_case_without_lower_string_method.py
@@ -0,0 +1,23 @@
+# lower() converts all characters of the string into lower case.
+# Create a program that do the same functionality without using lower() function.
+
+# set a variable with an element of string of characters
+string = "AcE fRaNcIs V. aGuStIn"
+
+# create an empty list for lower cased characters
+lower_cased = []
+
+# create a for loop to iterate all items in the given string
+for char in range(len(string)):
+# if char in string is in upper case,
+    if string[char].isupper():
+# change the case of the char and add to the list
+        swapped_case = string[char].swapcase()
+        lower_cased.append(swapped_case)
+# if char is not in upper case,
+    else:
+# add char to the list
+        lower_cased.append(string[char])
+
+# print the string of characters in lower case
+print("Characters in lower case: ", "".join(lower_cased))
