a8dfe5fc4d967a0f86144bb1eea77f2e64dfee1c
AceFVA <acefrancisagustin@gmail.com>
2025-03-26 18:08:06 +0800

    Created the for loop for identifying left-padded spaces

--- simple_projects_in_python_6/program_for_removing_left_padded_spaces_without_lstrip_method.py
+++ simple_projects_in_python_6/program_for_removing_left_padded_spaces_without_lstrip_method.py
@@ -3,9 +3,16 @@
 
 # input any characters with left padded spaces
 input_str = input("Enter any characters with left-padded spaces: ")
+
 # initialize a variable at value of 0 as container for left-padded spaces
 left_spaces = 0
+
 # create a for loop that identifies the left-padded spaces
+for i in range(len(input_str)):
+    if input_str[i] == " ":
+        left_spaces += 1
+    else:
+        break
 
 # print the input without left-padded spaces
-print("Removed left-padded spaces: ")
+print("Removed left-padded spaces: ", input_str[left_spaces:])
