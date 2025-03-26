78cb8a19a611a47bb66e0f11beaf2ca2e6fd7705
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 01:21:12 +0800

    Added variables and if statement to identify if the end part of variable 1 matches woth variable 2

--- simple_projects_in_python_6/program_for_checking_end_part_of_a_string_without_endswith_method.py
+++ simple_projects_in_python_6/program_for_checking_end_part_of_a_string_without_endswith_method.py
@@ -2,6 +2,11 @@
 # Create a program that do the same functionality without using endswith() function.
 
 # create a variable with string of characters
+string = "Ace Francis V. Agustin"
 # create the function parameter
+end_part = "Agustin"
+
 # if the end part of the given string is equal to the function parameter,
+if string[-1:-(len(end_part)+1):-1] == end_part[::-1]:
 # print True
+    print("True")
