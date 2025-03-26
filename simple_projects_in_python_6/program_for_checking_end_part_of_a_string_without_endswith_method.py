5d228af459834836fb5403630cb7f7abd1cf3de0
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 01:17:34 +0800

    added pseucoode

---
+++ simple_projects_in_python_6/program_for_checking_end_part_of_a_string_without_endswith_method.py
@@ -0,0 +1,7 @@
+# endswith() check if the string end part matches the function parameter.
+# Create a program that do the same functionality without using endswith() function.
+
+# create a variable with string of characters
+# create the function parameter
+# if the end part of the given string is equal to the function parameter,
+# print True
