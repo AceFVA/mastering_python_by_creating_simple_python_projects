066549c97e259fec04834cac72cafed09cc10889
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 20:04:03 +0800

    Crwated for loop with if-else statement for character case identification

--- simple_projects_in_python_6/program_for_swapping_cases_without_swapcase_method.py
+++ simple_projects_in_python_6/program_for_swapping_cases_without_swapcase_method.py
@@ -8,18 +8,19 @@ string = "AcE fRaNcIs V. aGuStIn"
 swapped_string = ""
 
 # iterate all characters in the given string
-
+for char in range(len(string)):
 # if character is in uppercase,
-
+    if string[char].isupper():
 # swapped string variable += character in lowercase
-
+        swapped_string += string[char].lower()
 # if character is not alnum,
-
+    elif not(string[char].isalnum()):
 # swapped string variable += character
-
+        swapped_string += string[char]
 # if character is in lowercase,
-
+    else:
 # swapped string variable += character in uppercase
+        swapped_string += string[char].upper()
 
 # print string in swapped case
 print("String in swapped cases: ", swapped_string)
