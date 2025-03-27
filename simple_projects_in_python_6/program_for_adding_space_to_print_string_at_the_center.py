0d8d0b147d2333aebf9e5fa1d89f17c2cf4a8827
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 19:38:42 +0800

    Added the finished code for center()-like program

--- simple_projects_in_python_6/program_for_adding_space_to_print_string_at_the_center.py
+++ simple_projects_in_python_6/program_for_adding_space_to_print_string_at_the_center.py
@@ -2,8 +2,19 @@
 # Create a program that do the same functionality without using center() function.
 
 # set a variable with string element
+string = "Ace"
+
 # set a variabel with the specified function parameter
+length = 9
+
 # find the number of remaining characters needed to fulfill the funciton parameter 
+space_added = 9 - len(string)
+
 # [space string] * (remaining space needed // 2) + string + [space string] * (remaining space needed // 2)
+centered_string = (" " * (space_added // 2)) + string + (" " * (space_added // 2))
+
 # print string at the center
 # print string length for verification
+print("String at the center: ", centered_string)
+print("String length: ", len(centered_string))
+
