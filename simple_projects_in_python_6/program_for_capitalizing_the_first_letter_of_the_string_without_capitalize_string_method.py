ff53c461df8e778959059b2be1eaba64bd192c05
AceFVA <acefrancisagustin@gmail.com>
2025-03-28 00:22:11 +0800

    Added initial lines of code (incomplete

--- simple_projects_in_python_6/program_for_capitalizing_the_first_letter_of_the_string_without_capitalize_string_method.py
+++ simple_projects_in_python_6/program_for_capitalizing_the_first_letter_of_the_string_without_capitalize_string_method.py
@@ -2,8 +2,11 @@
 # Create a program that do the same functionality without using capitalize() function.
 
 # set a variable with a string element
+string = ".!? ace francis v. agustin"
+
 # iterate all characters in the given string
 # check if the iterated character is a letter
 # replace the first alphabet character by its capitilized case
 # end loop if the first letter was capitalized
 # print string in capitalized case
+print(capitalized_string)
