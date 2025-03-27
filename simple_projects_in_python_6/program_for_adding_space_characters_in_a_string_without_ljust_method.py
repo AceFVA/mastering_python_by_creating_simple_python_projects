3da5fb1c8160c3d53e81f49bf5015ee5c154ca2b
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 18:51:29 +0800

    Added complete code for making the ljust() function

--- simple_projects_in_python_6/program_for_adding_space_characters_in_a_string_without_ljust_method.py
+++ simple_projects_in_python_6/program_for_adding_space_characters_in_a_string_without_ljust_method.py
@@ -2,7 +2,17 @@
 # Create a program that do the same functionality without using ljust() function.
 
 # set a variable with string element
+string = "Ace"
+
 # set a variable with the desired string length
+length = 9
+
 # find the number of space to be added to fulfill the remaining characters
+space_added = 9 - len(string)
+
 # string + [space string] * remaining characters needed
+with_spaces = string + (" " * space_added)
+
 # print the string with spaces added and its length
+print("String with spaces: ", with_spaces)
+print("String length: ", len(with_spaces))
