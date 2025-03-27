aa4fdd19db3a9edc12f18b445e38a0e51bc96ffe
AceFVA <acefrancisagustin@gmail.com>
2025-03-28 00:20:18 +0800

    Added pseudocode

---
+++ simple_projects_in_python_6/program_for_capitalizing_the_first_letter_of_the_string_without_capitalize_string_method.py
@@ -0,0 +1,9 @@
+# capitalize() makes the first letter of the string, capital letter. And all other letter in small case.
+# Create a program that do the same functionality without using capitalize() function.
+
+# set a variable with a string element
+# iterate all characters in the given string
+# check if the iterated character is a letter
+# replace the first alphabet character by its capitilized case
+# end loop if the first letter was capitalized
+# print string in capitalized case
