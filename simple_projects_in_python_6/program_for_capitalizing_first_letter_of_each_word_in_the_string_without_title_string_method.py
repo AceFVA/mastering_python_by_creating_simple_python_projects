63baad09a3e6ee6bf56a580753c41ff015f33ba2
AceFVA <acefrancisagustin@gmail.com>
2025-03-28 01:15:22 +0800

    Created for loop to capitalized every first letter of each word

--- simple_projects_in_python_6/program_for_capitalizing_first_letter_of_each_word_in_the_string_without_title_string_method.py
+++ simple_projects_in_python_6/program_for_capitalizing_first_letter_of_each_word_in_the_string_without_title_string_method.py
@@ -11,12 +11,16 @@ word_list = string.split()
 in_title_case = []
 
 # iterate all letters in each words
-
+for word in word_list:
+    for letter in word:
 # if letter is an lphabet character
-
+        if letter.isalpha():
 # replace letter by its capital casing
-
+            word = "".join(word.replace(letter, letter.upper(), 1))
+# add title-cased word to the list
+            in_title_case.append(word)
 # end the loop if the first letter was capitalized
+            break
 
 # print the string in title case
-print(in_title_case)
+print("String in title case:", " ".join(in_title_case))
