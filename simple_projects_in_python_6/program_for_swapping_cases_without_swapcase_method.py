36dd9c27ec05e8f2f4e3771f6163d5fd17d361bc
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 20:00:13 +0800

    Added defined variables for initial code

--- simple_projects_in_python_6/program_for_swapping_cases_without_swapcase_method.py
+++ simple_projects_in_python_6/program_for_swapping_cases_without_swapcase_method.py
@@ -2,12 +2,24 @@
 # Create a program that do the same functionality without using swapcase() function.
 
 # set a variable with string element
+string = "AcE fRaNcIs V. aGuStIn"
+
 # set a variabel with an empty string element
+swapped_string = ""
+
 # iterate all characters in the given string
+
 # if character is in uppercase,
+
 # swapped string variable += character in lowercase
+
 # if character is not alnum,
+
 # swapped string variable += character
+
 # if character is in lowercase,
+
 # swapped string variable += character in uppercase
+
 # print string in swapped case
+print("String in swapped cases: ", swapped_string)
