b7d6f82da9a1c50dfafd837ff5ce09c3dc1c1434
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 19:57:56 +0800

    Added pseuodocode

---
+++ simple_projects_in_python_6/program_for_swapping_cases_without_swapcase_method.py
@@ -0,0 +1,13 @@
+# swapcase() reverse the casing of each of the character of the string.
+# Create a program that do the same functionality without using swapcase() function.
+
+# set a variable with string element
+# set a variabel with an empty string element
+# iterate all characters in the given string
+# if character is in uppercase,
+# swapped string variable += character in lowercase
+# if character is not alnum,
+# swapped string variable += character
+# if character is in lowercase,
+# swapped string variable += character in uppercase
+# print string in swapped case
