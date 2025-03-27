9c14c458ee21393c8a263ad691ea1bed3eb1cffc
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 18:46:51 +0800

    Added pseudocode

---
+++ simple_projects_in_python_6/program_for_adding_space_characters_in_a_string_without_ljust_method.py
@@ -0,0 +1,8 @@
+# ljust() add space characters at the end of the string to complete the number of characters specifies in function parameter.
+# Create a program that do the same functionality without using ljust() function.
+
+# set a variable with string element
+# set a variable with the desired string length
+# find the number of space to be added to fulfill the remaining characters
+# string + [space string] * remaining characters needed
+# print the string with spaces added and its length
