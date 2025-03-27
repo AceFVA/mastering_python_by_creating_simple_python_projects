7372c2a68a7805e39a2c9090dc163b957cd644fd
AceFVA <acefrancisagustin@gmail.com>
2025-03-28 00:24:52 +0800

    created for loop to identify the first letter and make it in uppercase

--- simple_projects_in_python_6/program_for_capitalizing_the_first_letter_of_the_string_without_capitalize_string_method.py
+++ simple_projects_in_python_6/program_for_capitalizing_the_first_letter_of_the_string_without_capitalize_string_method.py
@@ -5,8 +5,13 @@
 string = ".!? ace francis v. agustin"
 
 # iterate all characters in the given string
+for char in string:
 # check if the iterated character is a letter
+    if char.isalpha():
 # replace the first alphabet character by its capitilized case
+        capitalized_string = string.replace(char, char.upper(), 1)
 # end loop if the first letter was capitalized
+        break
+
 # print string in capitalized case
 print(capitalized_string)
