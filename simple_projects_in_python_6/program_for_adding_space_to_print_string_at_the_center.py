5a6c5a85ed2896349f7367c5151ff6c0110f6b51
AceFVA <acefrancisagustin@gmail.com>
2025-03-27 19:33:16 +0800

    Added peudocode

---
+++ simple_projects_in_python_6/program_for_adding_space_to_print_string_at_the_center.py
@@ -0,0 +1,9 @@
+# center() add space characters at the beginning and at the end of the string to print the string at the center.
+# Create a program that do the same functionality without using center() function.
+
+# set a variable with string element
+# set a variabel with the specified function parameter
+# find the number of remaining characters needed to fulfill the funciton parameter 
+# [space string] * (remaining space needed // 2) + string + [space string] * (remaining space needed // 2)
+# print string at the center
+# print string length for verification
