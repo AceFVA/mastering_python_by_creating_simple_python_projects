844f93a1cda4d158977950ded77eb4d56a2fae01
AceFVA <acefrancisagustin@gmail.com>
2025-03-28 01:06:40 +0800

    Added initial lines of code (incomplete)

--- simple_projects_in_python_6/program_for_capitalizing_first_letter_of_each_word_in_the_string_without_title_string_method.py
+++ simple_projects_in_python_6/program_for_capitalizing_first_letter_of_each_word_in_the_string_without_title_string_method.py
@@ -2,10 +2,21 @@
 # Create a program that do the same functionality without using title() function.
 
 # set a variable with string element
+string = ".!?ace francis v. agustin"
+
 # split each word
+word_list = string.split()
+
 # create an empty list for title-cased words
+in_title_case = []
+
 # iterate all letters in each words
+
 # if letter is an lphabet character
+
 # replace letter by its capital casing
+
 # end the loop if the first letter was capitalized
+
 # print the string in title case
+print(in_title_case)
