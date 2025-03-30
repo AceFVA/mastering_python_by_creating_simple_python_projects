dbf39f96d730c4cd5330321826a7a9dfd4b52e70
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 01:28:03 +0800

    Added speudocode

---
+++ simple_projects_in_python_7/program_for_removing_suffix_characters_without_removesuffix_method.py
@@ -0,0 +1,8 @@
+# removesuffix() remove the characters at the end of the string that matches the function parameter.
+# Create a program that do the same functionality without using removesuffix() function.
+
+# set a variable with a string element
+# set the suffix to be removed
+# if the string ends with the given suffix
+# replace the characters with "" based on thr given suffix
+# print the string without the suffix
