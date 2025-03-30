d095dbe9a46710dcec1c3ed67b464550003bd50b
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 02:08:12 +0800

    added pseudocode

---
+++ simple_projects_in_python_7/program_for_checking_beginning_part_of_a_string_without_startswith_method.py
@@ -0,0 +1,7 @@
+# startswith() check if the string beginning part matches the function parameter.
+# Create a program that do the same functionality without using startswith() function.
+
+# set a variable with a string of characters
+# set a parameter
+# if the string begins with the given parameter
+# print True
