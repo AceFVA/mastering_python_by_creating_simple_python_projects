a5aa098a9ee187828fdea307426669d795460b5d
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 01:57:05 +0800

    added pseudocode

---
+++ simple_projects_in_python_7/program_for_checking_characters_if_lower_cased.py
@@ -0,0 +1,14 @@
+# islower() check if all characters of the string is on lower case.
+# Create a program that do the same functionality without using islower() function.
+
+# set a variable with a string of characters
+# set a variable with an initialized value of 0 for counting number of lowercased and alphanumeric characters
+# iterate all items in the given string
+# if char is not in uppercase,
+# lower case variable += 1
+# if char is in alphanumeric character,
+# lower case variable += 1
+# if the length of lower case variable equal to length of the string
+# print True
+# if the length of lower case variable not equal to length of the string,
+# print False
