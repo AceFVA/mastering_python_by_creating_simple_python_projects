e95db547e54058f0de0bcb7ce2bc1f3810cf1e9f
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 02:16:34 +0800

    Created an if statement to identify if the string starts with the given oarameter

--- simple_projects_in_python_7/program_for_checking_beginning_part_of_a_string_without_startswith_method.py
+++ simple_projects_in_python_7/program_for_checking_beginning_part_of_a_string_without_startswith_method.py
@@ -2,8 +2,16 @@
 # Create a program that do the same functionality without using startswith() function.
 
 # set a variable with a string of characters
+string = "Ace Francis V. Agustin"
+
 # set a parameter
+beginning_part = "Ace"
+
 # if the string begins with the given parameter
+if string[:len(beginning_part)] == beginning_part:
 # print True
+    print("True")
 # if the string does not begins with the given parameter,
+else:
 # print False
+    print("False")
