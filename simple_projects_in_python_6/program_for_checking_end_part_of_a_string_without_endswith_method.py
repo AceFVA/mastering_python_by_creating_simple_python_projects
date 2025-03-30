5b573c33435c6b6cccd1c68852bc316dfb3401a6
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 02:20:58 +0800

    Added false message if not ending with the function parameter

--- simple_projects_in_python_6/program_for_checking_end_part_of_a_string_without_endswith_method.py
+++ simple_projects_in_python_6/program_for_checking_end_part_of_a_string_without_endswith_method.py
@@ -10,3 +10,7 @@ end_part = "Agustin"
 if string[-1:-(len(end_part)+1):-1] == end_part[::-1]:
 # print True
     print("True")
+# if the end part of the given string is not equal to the function parameter
+else:
+# print False
+    print("False")
