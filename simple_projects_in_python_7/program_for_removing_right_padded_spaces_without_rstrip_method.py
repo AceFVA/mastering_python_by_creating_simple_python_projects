9e2d07579dc59bb275b6d581fd43e6e9c2e2d8f7
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 00:38:41 +0800

    Added pseudocode

---
+++ simple_projects_in_python_7/program_for_removing_right_padded_spaces_without_rstrip_method.py
@@ -0,0 +1,8 @@
+# rstrip() remove the space characters at the end of the string.
+# Create a program that do the same functionality without using rstrip() function.
+
+# set a variable with a string element
+# use negative indexing to indice string
+# use lstrip()
+# print the string without the right-padded spaces
+
