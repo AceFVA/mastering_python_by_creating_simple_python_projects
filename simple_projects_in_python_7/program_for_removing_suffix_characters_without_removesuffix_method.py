d5fa5a452baa83cf7e099967c38f5f8ac5d7abcc
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 01:30:45 +0800

    Created the code with endswith()

--- simple_projects_in_python_7/program_for_removing_suffix_characters_without_removesuffix_method.py
+++ simple_projects_in_python_7/program_for_removing_suffix_characters_without_removesuffix_method.py
@@ -2,7 +2,14 @@
 # Create a program that do the same functionality without using removesuffix() function.
 
 # set a variable with a string element
+string = "SB19 Ace SB19"
+
 # set the suffix to be removed
+suffix = "SB19"
+
 # if the string ends with the given suffix
+if string.endswith(suffix):
 # replace the characters with "" based on thr given suffix
+    removed_suffix = string[::-1].replace(suffix[::-1], "", 1)
 # print the string without the suffix
+print(removed_suffix[::-1])
