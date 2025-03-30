3c9a4c0f101833fd3ea0be2bc7cd713814dc4159
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 01:43:32 +0800

    Added initial line sof code (incomplete)

--- simple_projects_in_python_7/program_for_converting_characters_into_upper_case_without_upper_string_method.py
+++ simple_projects_in_python_7/program_for_converting_characters_into_upper_case_without_upper_string_method.py
@@ -2,10 +2,15 @@
 # Create a program that do the same functionality without using upper() function.
 
 # set a variable with a string of characters
+string = "AcE fRaNcIs V. aGuStIn"
+
 # create an empty list of uppercased letters
+upper_cased = []
+
 # iterate all items in the given string
 # if char in string is in lower case,
 # convert to upper case and add to the list
 # if char in string is not in lower case,
 # add char to the list
 # print the strinf in upper case
+print(upper_cased)
