165ed1db1d0711b43b7b1f477df74c131a52d096
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 15:47:47 +0800

    Added initial lines of code (incomplete)

--- simple_projects_in_python_7/program_for_finding_first_location_of_parameter_from_the_right_without_rindex_string_method.py
+++ simple_projects_in_python_7/program_for_finding_first_location_of_parameter_from_the_right_without_rindex_string_method.py
@@ -2,13 +2,25 @@
 # Create a program that do the same functionality without using rindex() function.
 
 # set a variable with a string od characters
+string = "Ace Francis V. Agustin"
+
 # set a variable for the function parameter
+parameter = "A"
+
 # find if there is a parameter present in the string
+locaiton = string.find(parameter)
+
 # create a dictionary for the location of every parameter found
+locations = {}
+
 # iterate all characters in the string
 # if character is equal to parameter,
 # add the char in the dictionary
 # if location is not equal to -1,
+if location != -1:
 # print the location of the parameter startinb from the right
+    print(max(locations.values()))
 # if location is equal to -1,
+else:
 # print ValueError
+    print("ValueError")
