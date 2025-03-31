6b45c7022ef65a798d161b4f666f7ace746fcf48
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 14:05:17 +0800

    Added pseudocode

---
+++ simple_projects_in_python_7/program_for_counting_occurence_of_function_parameter_without_count_string_method.py
@@ -0,0 +1,11 @@
+# count() return how many time the function parameter appear in the string.
+# Create a program that do the same functionality without using count() function.
+
+# set a variable with string of characters
+# set a variable for the specified function parameter
+# set a variable with an initial value of 0 for repetition counts
+# iterate all characters in the given string
+# if char is equal to the function parameter,
+# add char to the list
+# print the count of function parameter that is in the string
+
