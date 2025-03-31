f62727af642d31b09970aeeae3fc0f3df8d623ff
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 14:39:10 +0800

    Added pseudocode

---
+++ simple_projects_in_python_7/program_for_finding_first_location_of_parameter_withou_index_string_method.py
@@ -0,0 +1,9 @@
+# index() return the first location of the function parameter in the string.
+# Create a program that do the same functionality without using index() function.
+
+# set a variable with the string element
+# set a variable with the given parameter
+# find parameter in the given string
+# print the first location of the parameter
+# if output is -1,
+# print ValueError
