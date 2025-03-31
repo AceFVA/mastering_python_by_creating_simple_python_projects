5e7ccb814e351053a2692d51d5b69113045697c8
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 13:41:41 +0800

    Added pseudocodw

---
+++ simple_projects_in_python_7/program_for_adding_space_characters_in_a_string_without_rjust_method.py
@@ -0,0 +1,9 @@
+# rjust() add space characters at the beginning of the string to complete the number of characters specifies in function parameter.
+# Create a program that do the same functionality without using rjust() function.
+
+# set a variable with string element
+# set a variable for the specified function parameter
+# identify the number of spaces to be added to satisfy the given parameter
+# add space to the beginning of the string
+# print the string with the beginning spaces
+
