714447c35ed74c18bcf8912e3f336d09cb8e6b96
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 13:44:25 +0800

    Added the code to create a rjust-like program

--- simple_projects_in_python_7/program_for_adding_space_characters_in_a_string_without_rjust_method.py
+++ simple_projects_in_python_7/program_for_adding_space_characters_in_a_string_without_rjust_method.py
@@ -2,8 +2,16 @@
 # Create a program that do the same functionality without using rjust() function.
 
 # set a variable with string element
-# set a variable for the specified function parameter
-# identify the number of spaces to be added to satisfy the given parameter
-# add space to the beginning of the string
-# print the string with the beginning spaces
+string = "Ace"
 
+# set a variable for the specified function parameter
+parameter = 9
+
+# identify the number of spaces to be added to satisfy the given parameter
+space_added = 9 - len(string)
+
+# add space to the beginning of the string
+with_space = " " * space_added + string
+
+# print the string with the beginning spaces
+print("String with beginning spaces: ", with_space)
