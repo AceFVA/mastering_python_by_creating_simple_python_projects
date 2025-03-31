a09f4a5390f9b98a932b33edf245b67e3d4c9bb3
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 15:29:11 +0800

    added pseudocode

---
+++ simple_projects_in_python_7/program_for_finding_first_location_of_parameter_from_the_right_without_rindex_string_method.py
@@ -0,0 +1,15 @@
+# rindex() return the first location of the function parameter in the string starting from the last character.
+# Create a program that do the same functionality without using rindex() function.
+
+# set a variable with a string od characters
+# set a variable for the function parameter
+# set a variabale with initial value of -1 to for displaying "ValueError" message in case parameter is not found 
+# create a dictionary for the location of every parameter found
+# iterate all characters in the string
+# if character is equal to parameter,
+# find the location of the parameter on the string
+# add the char in the dictionary
+# if location is not equal to -1,
+# print the location of the parameter startinb from the right
+# if location is equal to -1,
+# print ValueError
