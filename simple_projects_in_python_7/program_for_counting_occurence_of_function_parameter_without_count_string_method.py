ed94cabdda315df2763872efc0a176544570d7c1
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 14:23:25 +0800

    Added initial line sof codes (incomplete)

--- simple_projects_in_python_7/program_for_counting_occurence_of_function_parameter_without_count_string_method.py
+++ simple_projects_in_python_7/program_for_counting_occurence_of_function_parameter_without_count_string_method.py
@@ -2,10 +2,16 @@
 # Create a program that do the same functionality without using count() function.
 
 # set a variable with string of characters
+string = "MAHALIMA"
+
 # set a variable for the specified function parameter
+parameter = "A"
+
 # set a variable with an initial value of 0 for repetition counts
+parameter_count = 0
+
 # iterate all characters in the given string
 # if char is equal to the function parameter,
 # add char to the list
 # print the count of function parameter that is in the string
-
+print(parameter_count)
