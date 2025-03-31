d6d11b595a3447407e50add6c29d82357905b5d5
AceFVA <acefrancisagustin@gmail.com>
2025-03-31 13:53:50 +0800

    Added the complete code to create a program that do just like zfill()

--- simple_projects_in_python_7/program_for_adding_zeros_without_zfill_string_method.py
+++ simple_projects_in_python_7/program_for_adding_zeros_without_zfill_string_method.py
@@ -2,7 +2,17 @@
 # Create a program that do the same functionality without using zfill() function.
 
 # set a variable with string of characters
+string = "Ace"
+
 # set a variable with the specified parameter
+parameter = 9
+
 # identify the number of zeros to be added to fulfill the parameter given
+zeros_added = parameter - len(string)
+
 # add zeros at the beginning of the string
+with_zeros = "0" * zeros_added + string
+
 # print the string with the zeros added
+print("String with zeros: ", with_zeros)
+
