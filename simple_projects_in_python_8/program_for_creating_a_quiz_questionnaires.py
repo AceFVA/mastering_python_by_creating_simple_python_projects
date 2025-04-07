9eafaf864af7a35eeced11d44838270993c48d76
AceFVA <acefrancisagustin@gmail.com>
2025-04-07 19:29:03 +0800

    Added pseudocode

---
+++ simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
@@ -0,0 +1,9 @@
+# Quiz Creator
+
+# open a new file
+# create a library for the questions, choices and answers to be inputted by user
+# ask the user to input a question
+# ask the user the four possible answer
+# ask the user to input the correct answer
+# collect all the inputs to the file
+# close the file
