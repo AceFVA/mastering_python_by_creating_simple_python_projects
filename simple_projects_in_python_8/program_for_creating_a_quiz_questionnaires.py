2745ccb4da3cf7dcfcb5c69ef77abdcc80390e2b
AceFVA <acefrancisagustin@gmail.com>
2025-04-07 23:31:19 +0800

    Added: New 'question' dictionary that will contain the question, choices, and answer. Created new for loop for changing the name of 'quesionquestion dictionary per user input
    Additional Changes: - Removed the first  inside the library - Adjusted the variable names in every for loop

--- simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
+++ simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
@@ -8,23 +8,24 @@ num = int(input("How many questions?: ")) # asks the user the number of question
 
 # ask the user to input a question
 while num:
-	main_questionnaire_dict["Questions"] = {}
+	for h in range(num):
+		main_questionnaire_dict[f"Question {h + 1}"] = {}
 
 	for i in range(num):
 		question = input("What is your question?: ")
-		main_questionnaire_dict["Questions"][f"Q{i + 1}"] = question
+		main_questionnaire_dict[f"Question {i + 1}"][f"Q{i + 1}"] = question
 
 		print("What are the four possible  answers?: ")
-		main_questionnaire_dict["Questions"][f"Choices{i + 1}"] = {}
+		main_questionnaire_dict[f"Question {i + 1}"][f"Choices{i + 1}"] = {}
 
 		# ask the user for four possible answers
 		for j in range(1, 5):
 			choices = input(f"Choice {j}: ")
-			main_questionnaire_dict["Questions"][f"Choices{i + 1}"][f"C{j}"] = choices
+			main_questionnaire_dict[f"Question {i + 1}"][f"Choices{i + 1}"][f"C{j}"] = choices
 
 		#ask the user to input the correct answer
 		answer = input("What is the correct answer?: ")
-		main_questionnaire_dict["Questions"][f"Answer{i + 1}"] = answer
+		main_questionnaire_dict[f"Question {i + 1}"][f"Answer{i + 1}"] = answer
 	print(main_questionnaire_dict)
 
 # collect all the inputs to the file
