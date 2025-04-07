84930150264092f428566f0c2c3fff79ddd299fd
AceFVA <acefrancisagustin@gmail.com>
2025-04-07 21:41:07 +0800

    Source code moved to the proper location

--- simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
+++ simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
@@ -1,12 +1,19 @@
 # Quiz Creator
 
 # open a new file
-# create a library for the questions, choices and answers to be inputted by user
-main_questionnaire_lib = {}
+# create a dictionary for the questions, choices and answers to be inputted by user
+main_questionnaire_dict = {}
 
-num = int(input("How many questions?: ") # asks the user the number of questions to be inputted
+num = int(input("How many questions?: ")) # asks the user the number of questions to be inputted
 
 # ask the user to input a question
+while num:
+        main_questionnaire_dict["Questions"] = {}
+
+        for i in range(num):
+                question = input("What is your question?: ")
+                main_questionnaire_dict["Questions"][f"Q{i + 1}"] = question
+
 # ask the user the four possible answer
 # ask the user to input the correct answer
 # collect all the inputs to the file
