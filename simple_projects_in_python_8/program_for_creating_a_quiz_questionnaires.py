55b0f396f620ed72401cae3e078be6988f4f7a91
AceFVA <acefrancisagustin@gmail.com>
2025-04-07 22:54:31 +0800

    Added: Created an additional for loop for the possible answers to inputted by the user, along with a dictionary for its container.
    Additional Changes: -Fixed the indentation level. -Added print() to verify the values inside the main dictionary

--- simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
+++ simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
@@ -8,12 +8,20 @@ num = int(input("How many questions?: ")) # asks the user the number of question
 
 # ask the user to input a question
 while num:
-        main_questionnaire_dict["Questions"] = {}
+	main_questionnaire_dict["Questions"] = {}
 
-        for i in range(num):
-                question = input("What is your question?: ")
-                main_questionnaire_dict["Questions"][f"Q{i + 1}"] = question
+	for i in range(num):
+		question = input("What is your question?: ")
+		main_questionnaire_dict["Questions"][f"Q{i + 1}"] = question
 
+		print("What are the four possible  answers?: ")
+		main_questionnaire_dict["Questions"][f"Choices{i + 1}"] = {}
+
+		for j in range(1, 5):
+			choices = input(f"Choice {j}: ")
+			main_questionnaire_dict["Questions"][f"Choices{i + 1}"][f"C{j + 1}"] = choices
+
+	print(main_questionnaire_dict)
 # ask the user the four possible answer
 # ask the user to input the correct answer
 # collect all the inputs to the file
