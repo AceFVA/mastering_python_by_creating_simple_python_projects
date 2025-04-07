0ed06c6a0cb7c2c13ad90789640628d94e27ae14
AceFVA <acefrancisagustin@gmail.com>
2025-04-08 00:20:35 +0800

    Added: Changed num to question_num. Added input() for asking if the user wants to continue adding questions.
    Additional Changes: - Removed two for loops since adding questions is not possible with the last structure of the source code. - Adjusted indentation level - Changed while num to while True - Readjusted the for loop variable

--- simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
+++ simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
@@ -1,32 +1,41 @@
 # Quiz Creator
 
 # open a new file
+
+
 # create a dictionary for the questions, choices and answers to be inputted by user
 main_questionnaire_dict = {}
 
-num = int(input("How many questions?: ")) # asks the user the number of questions to be inputted
+question_num = 1
 
 # ask the user to input a question
-while num:
-	for h in range(num):
-		main_questionnaire_dict[f"Question {h + 1}"] = {}
+while True:
+	main_questionnaire_dict[f"Question {question_num}"] = {}
 
-	for i in range(num):
-		question = input("What is your question?: ")
-		main_questionnaire_dict[f"Question {i + 1}"][f"Q{i + 1}"] = question
+	question = input("What is your question?: ")
+	main_questionnaire_dict[f"Question {question_num}"][f"Q{question_num}"] = question
 
-		print("What are the four possible  answers?: ")
-		main_questionnaire_dict[f"Question {i + 1}"][f"Choices{i + 1}"] = {}
+	print("What are the four possible  answers?: ")
+	main_questionnaire_dict[f"Question {question_num}"][f"Choices{question_num}"] = {}
 
-		# ask the user for four possible answers
-		for j in range(1, 5):
-			choices = input(f"Choice {j}: ")
-			main_questionnaire_dict[f"Question {i + 1}"][f"Choices{i + 1}"][f"C{j}"] = choices
+	# ask the user for four possible answers
+	for i in range(1, 5):
+		choices = input(f"Choice {i}: ")
+		main_questionnaire_dict[f"Question {question_num}"][f"Choices{question_num}"][f"C{i}"] = choices
 
-		#ask the user to input the correct answer
-		answer = input("What is the correct answer?: ")
-		main_questionnaire_dict[f"Question {i + 1}"][f"Answer{i + 1}"] = answer
+	#ask the user to input the correct answer
+	answer = input("What is the correct answer?: ")
+	main_questionnaire_dict[f"Question {question_num}"][f"Answer{question_num}"] = answer
 	print(main_questionnaire_dict)
+
+	# ask if the user wants to continue adding questions
+	additional_questions = input("Do you wish to continue adding questions? (Y/N): ")
+
+	if additional_questions.upper() != "Y":
+		break
+
+	question_num += 1
 
 # collect all the inputs to the file
 # close the file
+
