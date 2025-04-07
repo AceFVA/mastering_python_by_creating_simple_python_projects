99409ad4d3875d3ee4c773bf44c93c09aba784a5
AceFVA <acefrancisagustin@gmail.com>
2025-04-07 19:40:15 +0800

    Added a library for questions, choices, and answers, and ask the user the initial number of questions to be inputted

--- simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
+++ simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
@@ -2,6 +2,10 @@
 
 # open a new file
 # create a library for the questions, choices and answers to be inputted by user
+main_questionnaire_lib = {}
+
+num = int(input("How many questions?: ") # asks the user the number of questions to be inputted
+
 # ask the user to input a question
 # ask the user the four possible answer
 # ask the user to input the correct answer
