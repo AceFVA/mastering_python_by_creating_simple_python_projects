1be62c3e3538273b16102696101713d157425806
AceFVA <acefrancisagustin@gmail.com>
2025-04-15 16:16:01 +0800

    Added: while loop for continuous asking of selecting an option to add or remove questions, or save and exit
    Additional Changes: - Renamed file name - Edited display message - displays the current questions inputted when removing a question

--- simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
+++ simple_projects_in_python_8/program_for_creating_a_quiz_questionnaires.py
@@ -4,7 +4,7 @@
 import json
 
 # open a new file
-questionnaire_file = open("questionnaires.json", "w")
+questionnaire_file = open("quiz.json", "w")
 
 # create a dictionary for the questions, choices and answers to be inputted by user
 main_questionnaire_dict = {}
@@ -39,43 +39,60 @@ while True:
 	answer = input("What is the correct answer?: ")
 	main_questionnaire_dict[f"Question {question_num}"][f"Answer{question_num}"] = answer
 
-	# ask if the user wants to add, remove, or edit questions
-	print("\nDo you want to add, remove or edit questions?")
+	# ask if the user wants to add or remove questions
+	print("\nDo you want to add or remove questions?")
 	print("1. Add questions")
 	print("2. Remove questions")
 	print("3. Save and exit")
 
-	selected_opt = int(input("\nPlease select an option (1-3): "))
+	while True:
+		selected_opt = input("\nPlease select an option (1-3): ")
 
-	
-	# if selected_opt is equal to 1,
-	if selected_opt == 1:			
-		# question_num += 1
-		question_num += 1
+		# verify if input is invalid
+		if not(selected_opt.isdigit()):
+			print("Invalid input. Try again.")
+		
+		elif int(selected_opt) < 1 or int(selected_opt) > 3:
+			print("Choose only between 1 to 3.")
 
-	# if selected_opt is equal to 2,
-	elif selected_opt == 2:
-		# ask the user which question to be removed
-		question_removal = input("Which question do you want to remove? (e.g. Question 1): ")
+		# if selected_opt is equal to 1,
+		elif int(selected_opt) == 1:			
+			# question_num += 1
+			question_num += 1
+			break
 
-		# verify if the question exist in the dictionary
-		if question_removal in main_questionnaire_dict:
-			main_questionnaire_dict.pop(question_removal)
-			print(f"{question_removal} has been removed.")
+		# if selected_opt is equal to 2,
+		elif int(selected_opt) == 2:
+			# display the current questions added
+			print("\nCurrent questions in the questionnaire:")
+			for key, value in main_questionnaire_dict.items():
+				print(f"{key}:")
+				print(f"  Question: {value['Q' + key.split()[-1]]}")
+				print("  Choices:")
+				for choice_key, choice_value in value[f"Choices{key.split()[-1]}"].items():
+					print(f"    {choice_key}: {choice_value}")
+				print(f"  Answer: {value[f'Answer{key.split()[-1]}']}")
+
+			# ask the user which question to be removed
+			question_removal = input("Which question do you want to remove? (e.g. Question 1): ")
+
+			# verify if the question exist in the dictionary
+			if question_removal in main_questionnaire_dict:
+				main_questionnaire_dict.pop(question_removal)
+				print(f"{question_removal} has been removed.")
 			
-		else:
-			print(f"{question_removal} does not exist.")
+			else:
+				print(f"{question_removal} does not exist.")
 	
-	# if selected_opt is equal to 3,
-	elif selected_opt == 3:
-		# print that all questions have been saved
-		print("Your questions have been saved")
+		# if selected_opt is equal to 3,
+		elif int(selected_opt) == 3:
+			# print that all questions have been saved
+			print("Your questions have been saved")
+			question_num = 0
+			break
+
+	if question_num == 0:
 		break
-
-	else:
-		print("Invalid input. Proceeding to exit...\nAll questions have been saved.")
-
-
 
 	'''if additional_questions.upper() != "Y":
 		question_removal = input("Do you want to remove a question? (Y/N): ")
@@ -115,4 +132,4 @@ json.dump(main_questionnaire_dict, questionnaire_file, indent = 4)
 # close the file
 questionnaire_file.close()
 
-print("Thank you! You questions have been added to questionnaires.json")
+print("Thank you! You questions have been added to quiz.json")
