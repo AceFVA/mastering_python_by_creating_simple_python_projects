776d9328572acf4244429822f2c99b6b40e587bf
AceFVA <acefrancisagustin@gmail.com>
2025-04-26 16:31:48 +0800

    Added initial pseudocode

---
+++ simple_projects_in_python_8/program_for_creating_a_quiz_generator.py
@@ -0,0 +1,10 @@
+# Quiz of Wits
+
+# import necessary libraries
+# open the file created in Quiz Creator program in read mode
+# create a menu for the quiz
+# ask the user to select an option
+# ask the user to input their name for saving scores
+# let the user answer the questions
+# count the score
+# save the score to a file for the leaderboard
