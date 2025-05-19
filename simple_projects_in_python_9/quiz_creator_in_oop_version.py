f4fcae7a558148a2be612fa9a7cf8949794cd304
AceFVA <acefrancisagustin@gmail.com>
2025-05-20 00:31:30 +0800

    Added: imported QuestionDictionary, adding questions to the dictionary.
    Additional Changes: Added an option if user wants to continue adding question. / Adjusted the constructor

--- simple_projects_in_python_9/quiz_creator_in_oop_version.py
+++ simple_projects_in_python_9/quiz_creator_in_oop_version.py
@@ -2,11 +2,13 @@
 
 # import other classes
 from new_question import NewQuestion
+from questionnaire_dictionary import QuestionDictionary
 
 # main class
-class QuizCreator(NewQuestion):
+class QuizCreator(NewQuestion, QuestionDictionary):
     def __init__(self):
-        super().__init__()
+        NewQuestion.__init__(self)
+        QuestionDictionary.__init__(self)
 
     def main_menu(self):
         welcome_msg = "Welcome to Quiz Creator!"
@@ -21,13 +23,41 @@ class QuizCreator(NewQuestion):
     def asking_questions(self):
         while True:
             # ask the user to input the question
-            user_question = self.question()
+            user_ques = self.question()
 
             # ask the user to input the four possible answers
             user_q_choices = self.choice()
 
             # ask the user to input the correct answer
             user_q_answer = self.answer()
+
+            # add the question to the questionnaire dictionary
+            self.add_question(user_ques, user_q_choices, user_q_answer)
+            self.add_question_to_dict()
+        
+            # ask the user if they want to add another question
+            add_another_question = input("Do you want to add another question? (Y/N): ")
+            if add_another_question.strip().upper() == "N":
+                print("Thank you for using Quiz Creator!")
+                break
+
+            elif add_another_question.strip().upper() == "Y":
+                print("Let's add another question!\n")
+                continue
+
+        return self.main_questionnaire_dict
+
+# FOR TESTING PURPOSES ONLY
+
+sample_quiz = QuizCreator()
+# create an instance of the class
+sample_quiz.main_menu()
+# create a new question
+sample_quiz.asking_questions()
+
+print("Your questionnaire dictionary is ready!")
+# print the questionnaire dictionary
+print(sample_quiz.main_questionnaire_dict)
 
 # save the question, answers and choices in a dictionary
 # save the dictionary in a file
