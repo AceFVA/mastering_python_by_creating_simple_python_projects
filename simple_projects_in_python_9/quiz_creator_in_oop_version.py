326d34461e57a5f87c2670a094dbfe857ed58d7c
AceFVA <acefrancisagustin@gmail.com>
2025-05-19 21:27:51 +0800

    Added pseudocode

---
+++ simple_projects_in_python_9/quiz_creator_in_oop_version.py
@@ -0,0 +1,9 @@
+# Quiz Creator in OOP Version
+
+# main class
+# import other classes
+# ask the user to input the question
+# ask the user to input the four possible answers
+# ask the user to input the correct answer
+# save the question, answers and choices in a dictionary
+# save the dictionary in a file
