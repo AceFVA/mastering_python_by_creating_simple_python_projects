120dead904cdceabe44af0c237b75c391990dba6
AceFVA <acefrancisagustin@gmail.com>
2025-05-19 21:43:29 +0800

    Added: Imprted NewQuestion class to the main quiz creator file
    Additonal Changes: Main class for quiz creator and defined new instance for main menu

--- simple_projects_in_python_9/quiz_creator_in_oop_version.py
+++ simple_projects_in_python_9/quiz_creator_in_oop_version.py
@@ -1,7 +1,23 @@
 # Quiz Creator in OOP Version
 
-# main class
 # import other classes
+from new_question import NewQuestion
+
+# main class
+class QuizCreator(NewQuestion):
+    def main_menu():
+        welcome_msg = "Welcome to Quiz Creator!"
+        print(welcome_msg.center(48))
+        print("\nHow this works?")
+        print("1. Enter a question")
+        print("2. Input the four possible answers to the question.")
+        print("3. Type the correct answer")
+        print("4. Enter and wait for the confirmation that the program is finish.\n")
+        print("Let's Start!\n")
+
+        main_questionnaire_dict = {}
+        question_num = 1
+
 # ask the user to input the question
 # ask the user to input the four possible answers
 # ask the user to input the correct answer
