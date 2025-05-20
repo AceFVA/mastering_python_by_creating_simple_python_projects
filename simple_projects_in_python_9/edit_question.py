a257364d4d1140eb6368ea0fb4debedb49a03842
AceFVA <acefrancisagustin@gmail.com>
2025-05-20 19:13:58 +0800

    Fixed indentation level

--- simple_projects_in_python_9/edit_question.py
+++ simple_projects_in_python_9/edit_question.py
@@ -6,34 +6,34 @@ class EditQuestion(NewQuestion, ViewQuestion, SaveQuestion):
     def __init__(self):
         super().__init__()
 
-def change_question(self):
-    self.file_name = input("Enter the quiz file name to edit (without .json): ")
-    loader = SaveQuestion(self.file_name)
-    self.main_questionnaire_dict = loader.load_questions()
+    def change_question(self):
+        self.file_name = input("Enter the quiz file name to edit (without .json): ")
+        loader = SaveQuestion(self.file_name)
+        self.main_questionnaire_dict = loader.loading_questions()
 
-    self.view_question()
+        self.view_question()
 
-    changing_question = input("Which question do you want to edit? (e.g. Question 1): ")
+        changing_question = input("Which question do you want to edit? (e.g. Question 1): ")
 
-    if changing_question.strip().capitalize() in self.main_questionnaire_dict:
-        # ask the user new question, choices, and answer
-        user_new_question = self.question()
-        user_new_q_choices = self.choice()
-        user_new_q_answer = self.answer()
+        if changing_question.strip().capitalize() in self.main_questionnaire_dict:
+            # ask the user new question, choices, and answer
+            user_new_question = self.question()
+            user_new_q_choices = self.choice()
+            user_new_q_answer = self.answer()
 
-        self.main_questionnaire_dict[changing_question.strip().capitalize()] = {
-            "Question": user_new_question,
-            "Choices": user_new_q_choices,
-            "Answer": user_new_q_answer
-        }
+            self.main_questionnaire_dict[changing_question.strip().capitalize()] = {
+                "Question": user_new_question,
+                "Choices": user_new_q_choices,
+                "Answer": user_new_q_answer
+            }
 
-        print(f"{changing_question} has been changed successfully!")
+            print(f"{changing_question} has been changed successfully!")
 
-        saver = SaveQuestion(self.file_name)
-        saver.saving_question(self.main_questionnaire_dict)
+            saver = SaveQuestion(self.file_name)
+            saver.saving_question(self.main_questionnaire_dict)
 
-    else:
-        print("Question does not exist.")
+        else:
+            print("Question does not exist.")
 
     def remove_question(self):
         self.view_question()
