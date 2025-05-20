6d43f2293334fe4b6affb4a4d5ca2262b404d01a
AceFVA <acefrancisagustin@gmail.com>
2025-05-20 18:52:27 +0800

    Added some space for printing questions, choices, and answers.

--- simple_projects_in_python_9/view_question.py
+++ simple_projects_in_python_9/view_question.py
@@ -7,9 +7,9 @@ class ViewQuestion(QuestionDictionary):
     def view_question(self):
         for key, value in self.main_questionnaire_dict.items():
             print(f"{key}: ")
-            print(f"{value['Question']}")
+            print(f"    {value['Question']}")
 
             for choice_key, choice_value in value['Choices'].items():
-                print(f"{choice_key}: {choice_value}")
+                print(f"        {choice_key}: {choice_value}")
 
-            print(f"Answer: {value['Answer']}")
+            print(f"    Answer: {value['Answer']}")
