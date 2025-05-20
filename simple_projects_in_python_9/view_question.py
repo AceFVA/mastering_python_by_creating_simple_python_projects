503f398b6801ceea63ed7265aeeaa479dfbbeba8
AceFVA <acefrancisagustin@gmail.com>
2025-05-20 13:48:17 +0800

    Added: New class for viewing questions, created intructor, and defined instance method for printing questions inside the dictionary.

---
+++ simple_projects_in_python_9/view_question.py
@@ -0,0 +1,15 @@
+from questionnaire_dictionary import QuestionDictionary
+
+class ViewQuestion(QuestionDictionary):
+    def __init__(self):
+        super().__init__()
+
+    def view_question(self):
+        for key, value in self.main_questionnaire_dict.items():
+            print(f"{key}: ")
+            print(f"{value['Question']}")
+
+            for choice_key, choice_value in value['Choices'].items():
+                print(f"{choice_key}: {choice_value}")
+
+            print(f"Answer: {value['Answer']}")
