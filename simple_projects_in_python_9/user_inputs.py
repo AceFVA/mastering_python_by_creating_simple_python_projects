869592ad81a513a7b239d432850afc59aa5fee67
AceFVA <acefrancisagustin@gmail.com>
2025-05-20 15:16:55 +0800

    Transferred the methods from QuizCreator to UserInputs to make QuizCreator(main.py) clean
    Additional Changes: added options for adding, editing, removing, and viewing questions, and exiting the program

---
+++ simple_projects_in_python_9/user_inputs.py
@@ -0,0 +1,40 @@
+import rich
+from rich.console import Console
+
+from new_question import NewQuestion
+from questionnaire_dictionary import QuestionDictionary
+from view_question import ViewQuestion
+from edit_question import EditQuestion
+
+console = Console()
+
+class UserInput(NewQuestion, QuestionDictionary, ViewQuestion, EditQuestion):
+    def __init__(self):
+        self.menu()
+
+    def menu(self):
+        console.rule("[bold yellow]welcome to Quiz Creator![/bold yellow]")
+
+        console.print("1. Add Questions")
+        console.print("2. Edit Questions")
+        console.print("3. Remove Questions")
+        console.print("4. View Questions")
+        console.print("5. Exit")
+
+        selected_option = int(input("Please select an option (1 - 5):"))
+
+        if selected_option == 1:
+            print("\nHow this works?")
+            print("1. Enter a question")
+            print("2. Input the four possible answers to the question.")
+            print("3. Type the correct answer")
+            print("4. Enter and wait for the confirmation that the program is finish.\n")
+            print("Let's Start!\n")
+
+            self.asking_questions()
+
+        elif selected_option == 2:
+            self.change_question()
+
+        elif selected_option == 3:
+            self.remove_question()
