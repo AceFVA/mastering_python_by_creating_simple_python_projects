351b9f945691ca2bcba5c7275841f0fc7e5bbac4
AceFVA <acefrancisagustin@gmail.com>
2025-05-23 14:12:41 +0800

    Imported SaveQuestion class to properly save the questionnaires on a json file and made some adjustments in methods.

--- simple_projects_in_python_9/view_question.py
+++ simple_projects_in_python_9/view_question.py
@@ -2,10 +2,12 @@ import os
 import json 
 
 from questionnaire_dictionary import QuestionDictionary
+from save_question import SaveQuestion 
 
-class ViewQuestion(QuestionDictionary):
+class ViewQuestion(QuestionDictionary, SaveQuestion):
     def __init__(self):
         super().__init__()
+        self.file_selected = None
 
     def view_question(self):
         for key, value in self.main_questionnaire_dict.items():
@@ -19,6 +21,7 @@ class ViewQuestion(QuestionDictionary):
 
     def select_file(self):
         selected_file = input("Enter the quiz file name to view (without .json): ")
+        self.file_name = selected_file
         self.file_selected = f"{selected_file}.json"
 
         if not os.path.exists(self.file_selected):
@@ -26,10 +29,16 @@ class ViewQuestion(QuestionDictionary):
             return False
 
         try:
-            with open(self.file_selected, "r") as file:
-                json.load(file)
-                print(f"Successfully loaded '{self.file_selected}'.")
+            questions_loader = SaveQuestion(self.file_name)
+            self.main_questionnaire_dict = questions_loader.loading_questions()
+
+            if self.main_questionnaire_dict:
+                print(f"Loaded questions from {self.file_selected}:")
                 return True
+            
+            else:
+                print("No questions found in the file.")
+                return False
 
         except json.JSONDecodeError:
             print("Error: The file is not in valid JSON format.")
