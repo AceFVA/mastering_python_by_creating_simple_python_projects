2625af92647795ec267e56a886958a5b4954e70d
AceFVA <acefrancisagustin@gmail.com>
2025-05-23 14:37:52 +0800

    Imported time for adding delays in displaying messages.

--- simple_projects_in_python_9/new_question.py
+++ simple_projects_in_python_9/new_question.py
@@ -1,4 +1,6 @@
 # Create a class for creating a new question
+import time
+
 class NewQuestion:
     def __init__(self, questions = None, choices = None, answers = None):
         super().__init__()
@@ -9,6 +11,7 @@ class NewQuestion:
     # Create a new question
     def question(self):
         self.user_question = input("What is your question?: ")
+        time.sleep(1)
         return self.user_question
 
     # Create four possible answers
@@ -17,7 +20,8 @@ class NewQuestion:
         choice = {}
         for i in range(1, 5):
             user_choices = input(f"Choice {i}: ")
-            choice[f"Choice {i}"] = user_choices
+            choice[f"Choice {i}"] = 
+            time.sleep(1)
 
         self.choices = choice
         return self.choices
@@ -25,4 +29,5 @@ class NewQuestion:
     # Create the correct answer
     def answer(self):
         self.ques_answer = input("What is the correct answer?: ")
+        time.sleep(1)
         return self.ques_answer
