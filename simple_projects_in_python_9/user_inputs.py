4e6bb05f562916bc5592bae46c1423aa1264a012
AceFVA <acefrancisagustin@gmail.com>
2025-05-26 16:47:29 +0800

    Added colors and timers

--- simple_projects_in_python_9/user_inputs.py
+++ simple_projects_in_python_9/user_inputs.py
@@ -24,21 +24,21 @@ class UserInput(EditQuestion):
         console.print("5. Exit")
 
     def user_selecting_option(self):
-        selected_option = int(input("\nPlease select an option (1 - 5): "))
+        selected_option = int(console.input("\n[green]Please select an option[/green] (1-5): "))
 
         try:
             if selected_option == 1:
-                print("\nHow this works?")
+                console.print("\n[yellow]How this works?[/yellow]")
                 time.sleep(1)
-                print("1. Enter a question")
+                console.print("1. Enter a question")
                 time.sleep(1)
-                print("2. Input the four possible answers to the question.")
+                console.print("2. Input the four possible answers to the question.")
                 time.sleep(1)
-                print("3. Type the correct answer")
+                console.print("3. Type the correct answer")
                 time.sleep(1)
-                print("4. Enter and wait for the confirmation that the program is finish.\n")
+                console.print("4. Enter and wait for the confirmation that the program is finish.\n")
                 time.sleep(1)
-                print("Let's Start!\n")
+                console.print("Let's Start!\n")
 
                 self.asking_questions()
                 self.user_saving_questions()
@@ -50,10 +50,12 @@ class UserInput(EditQuestion):
                     self.remove_question()
 
             elif selected_option == 4:
+                    console.print("[yellow]Loading questions...[yellow]")
+                    time.sleep(1)
                     self.view_question()
 
             elif selected_option == 5:
-                print("Thank you for using Quiz Creator!")
+                console.print("[yellow]Thank you for using Quiz Creator![/yellow]")
                 time.sleep(1)
                 exit()
 
@@ -61,64 +63,69 @@ class UserInput(EditQuestion):
             self.user_selecting_option()
 
         except TypeError:
-            print("Invalid input. Please enter a number between 1 and 5.")
+            console.print("[red]Invalid input.[/red] Please enter a number between 1 and 5.")
             self.menu()
             self.user_selecting_option()
 
         except ValueError:
-            print("Invalid input. Please enter a number between 1 and 5.")
+            console.print("[red]Invalid input.[/red] Please enter a number between 1 and 5.")
             self.menu()
             self.user_selecting_option()
 
         except KeyboardInterrupt:
-            print("\nExiting the program...")
+            console.print("\n[yellow]Exiting the program...[/yellow]")
             time.sleep(1)
             exit()
 
     def user_adding_questions(self):
         try:
             # ask the user if they want to add another question
-            add_another_question = input("Do you want to add another question? (Y/N): ")
+            add_another_question = console.input("\n[blue]Do you want to add another question?[/blue] [green](Y/N)[/green]: ").strip()
 
             if add_another_question.strip().upper() == "N":
                 return "break"
 
             elif add_another_question.strip().upper() == "Y":
-                print("Let's add another question!\n")
+                console.print("\n[yellow]Let's add another question![/yellow]\n")
                 return "continue"
             
             else:
-                print("Invalid input. Please enter Y or N.")
+                console.print("[red]Invalid input. Please enter Y or N.[red]")
                 return self.user_adding_questions()
         
         except KeyboardInterrupt:
-            print("\nExiting the program...")
+            console.print("\n[yellow]Exiting the program...[/yellow]")
             time.sleep(1)
             exit()
         
     def user_saving_questions(self):
         try:
-            user_decision = input("Do you want to save your questions? (Y/N): ")
+            user_decision = console.input("\n[blue]Do you want to save your questions?[/blue] [green](Y/N)[/green]: ").strip()
+            time.sleep(1)
 
             if user_decision.strip().upper() == "Y":
-                self.file_name = input("Type any file name you want: ").strip()
+                self.file_name = console.input("\n[green]Type any file name you want: [/green]").strip()
+                time.sleep(1)
 
+                console.print(f"\n[yellow]Saving your questions to {self.file_name}.json...[/yellow]")
+                time.sleep(1)
                 questions_saver = SaveQuestion(self.file_name)
                 questions_saver.saving_question(self.main_questionnaire_dict)
 
 
             elif user_decision.strip().upper() == "N":
-                print("Your questions were not saved.")
+                console.print("\n[red]Your questions were not saved.[/red]")
 
             else:
-                print("Invalid input. Please enter Y or N.")
+                console.print("\n[red]Invalid input.[/red] Please enter Y or N.")
                 self.user_saving_questions()
         
         except KeyboardInterrupt:
-            print("\nExiting the program...")
+            console.print("\n[yellow]Exiting the program...[/yellow]")
             time.sleep(1)
             exit()
         
         except OSError:
-            print("Error: Could not save the file.")
+            console.print("\n[red]Error: Could not save the file.[/red]")
+            time.sleep(1)
             return False
