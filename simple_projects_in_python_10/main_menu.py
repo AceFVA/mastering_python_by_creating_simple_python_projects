9c96f826b5dfcd15f1e46251ef6835c1e54227f5
AceFVA <acefrancisagustin@gmail.com>
2025-05-28 15:20:03 +0800

    Created a class with a constructor that contains the display_menu()

--- simple_projects_in_python_10/main_menu.py
+++ simple_projects_in_python_10/main_menu.py
@@ -3,44 +3,48 @@ import time
 
 from rich.console import Console
 
-def display_menu():
-    console.rule("Welcome to the [bold blue]Quiz of Wits![/bold blue]")
-    console.print("[blue]1.[/blue] Start Quiz")
-    console.print("[blue]2.[/blue] View Leaderboard")
-    console.print("[blue]3.[/blue] Exit")
+class MainMenu:
+    def __init__(self):
+        self.console = Console()
 
-    # ask the user to select an option
-    try:
-        selected_option = int(input("\nPlease select an option (1-3): "))
-        
-        if selected_option == 1:
-            # Start Quiz
-            start_quiz()
+    def display_menu(self):
+        self.console.rule("Welcome to the [bold blue]Quiz of Wits![/bold blue]")
+        self.console.print("[blue]1.[/blue] Start Quiz")
+        self.console.print("[blue]2.[/blue] View Leaderboard")
+        self.console.print("[blue]3.[/blue] Exit")
 
-        elif selected_option == 2:
-            # View Leaderboard
-            console.print("\n[yellow]Displaying the leaderboard...[/yellow]\n")
-            time.sleep(3)
-            leaderboard()
+        # ask the user to select an option
+        try:
+            self.selected_option = int(input("\nPlease select an option (1-3): "))
+            
+            if self.selected_option == 1:
+                # Start Quiz
+                self.start_quiz()
 
-        elif selected_option == 3:
-            # Exit
-            console.print("\n[yellow]Exiting the quiz...[/yellow]")
+            elif self.selected_option == 2:
+                # View Leaderboard
+                self.console.print("\n[yellow]Displaying the leaderboard...[/yellow]\n")
+                time.sleep(3)
+                self.leaderboard()
+
+            elif self.selected_option == 3:
+                # Exit
+                self.console.print("\n[yellow]Exiting the quiz...[/yellow]")
+                time.sleep(1)
+                self.console.print("\n[yellow]Goodbye![/yellow]")
+                exit()
+
+            else:
+                self.console.print("\n[red]Invalid option.[/red] Please try again.")
+                self.display_menu()
+
+        except KeyboardInterrupt:
+            self.console.print("\n[red]Input interrupted by the user.[/red]")
+            self.console.print("\n[yellow]Exiting the quiz...[/yellow]")
             time.sleep(1)
-            console.print("\n[yellow]Goodbye![/yellow]")
+            self.console.print("\n[yellow]Goodbye![/yellow]")
             exit()
-
-        else:
-            console.print("\n[red]Invalid option.[/red] Please try again.")
-            display_menu()
-
-    except KeyboardInterrupt:
-        console.print("\n[red]Input interrupted by the user.[/red]")
-        console.print("\n[yellow]Exiting the quiz...[/yellow]")
-        time.sleep(1)
-        console.print("\n[yellow]Goodbye![/yellow]")
-        exit()
-        
-    except ValueError:
-        console.print("\n[red]Invalid input.[/red] Please enter a number between 1 and 3.")
-        display_menu()
+            
+        except ValueError:
+            self.console.print("\n[red]Invalid input.[/red] Please enter a number between 1 and 3.")
+            self.display_menu()
